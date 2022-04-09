148250c2aea2380af8eaca16e334e2b4e14d09c7
kbroniek <karol.broniek.gtm@gmail.com>
2022-04-09 09:15:12 +0200

    Add B2C API

--- src/LeaveSystem.Api/LeaveSystem.Api/Program.cs
+++ src/LeaveSystem.Api/LeaveSystem.Api/Program.cs
@@ -1,3 +1,4 @@
+using LeaveSystem.Shared;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
@@ -7,8 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
 builder.Services.AddAuthorization();
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,20 +24,30 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseWebAssemblyDebugging();
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.UseCors(builder => builder
-     .AllowAnyOrigin()
-     .AllowAnyMethod()
-     .AllowAnyHeader());
+    //app.UseCors(builder => builder
+    // .AllowAnyOrigin()
+    // .AllowAnyMethod()
+    // .AllowAnyHeader());
 }
 
 app.UseHttpsRedirection();
 
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
 app.UseAuthentication();
 app.UseAuthorization();
 
-var scopeRequiredByApi = app.Configuration["AzureAd:Scopes"];
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+var scopeRequiredByApi = app.Configuration["AzureAdB2C:Scopes"];
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -57,8 +71,3 @@ app.MapGet("/weatherforecast", (HttpContext httpContext) =>
 .RequireAuthorization();
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- src/LeaveSystem.Web/LeaveSystem.Web/Program.cs
+++ src/LeaveSystem.Web/LeaveSystem.Web/Program.cs
@@ -1,22 +1,32 @@
 using GoldenEye.Registration;
 using LeaveSystem.Web;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http;
 using Tickets;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+const string ApiName = "LeaveSystem.ServerAPI";
+
+builder.Services.AddHttpClient(ApiName, client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient(ApiName));
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMsalAuthentication(options =>
 {
-    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("User.Read");
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://leavesystem.onmicrosoft.com/4f24b978-403f-47fe-9cae-52deea03661d/API.Access");
 });
-builder.Services.AddDDD();
-builder.Services.AddAllDDDHandlers();
-builder.Services.AddLeaveSystemModule(builder.Configuration);
+//builder.Services.AddDDD();
+//builder.Services.AddAllDDDHandlers();
+//builder.Services.AddLeaveSystemModule(builder.Configuration);
 
 await builder.Build().RunAsync();
