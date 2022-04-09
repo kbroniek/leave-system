2e0372ca9631441f84ae5f35daf82bdb79dba1c1
kbroniek <karol.broniek.gtm@gmail.com>
2022-04-09 08:17:34 +0200

    Configure B2C

--- src/LeaveSystem.Api/LeaveSystem.Api/Program.cs
+++ src/LeaveSystem.Api/LeaveSystem.Api/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddAuthorization();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -21,6 +22,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors(builder => builder
+     .AllowAnyOrigin()
+     .AllowAnyMethod()
+     .AllowAnyHeader());
 }
 
 app.UseHttpsRedirection();
