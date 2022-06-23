2ff796455ae301ccad0a8794ee707f18cba6230b
kbroniek <karol.broniek.gtm@gmail.com>
2022-06-23 16:06:04 +0200

    Add fluent validator

---
+++ src/LeaveSystem.Web/LeaveSystem.Web/Pages/CreatingLeaveRequest/CreateLeaveRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaveSystem.Web.Pages.CreatingLeaveRequest;
+
+public class CreateLeaveRequestDto
+{
+    public DateTime DateFrom { get; set; }
+    public DateTime DateTo { get; set; }
+    public int? Hours { get; set; }
+    public Guid? Type { get; set; }
+    public string? Remarks { get; set; }
+}
---
+++ src/LeaveSystem.Web/LeaveSystem.Web/Pages/CreatingLeaveRequest/CreateLeaveRequestValidaor.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace LeaveSystem.Web.Pages.CreatingLeaveRequest;
+public class CreateLeaveRequestValidaor : AbstractValidator<CreateLeaveRequestDto>
+{
+    public CreateLeaveRequestValidaor()
+    {
+        RuleFor(p => p.DateFrom)
+            .NotNull().WithMessage("You must enter the date from");
+        RuleFor(p => p.DateTo)
+            .NotNull().WithMessage("You must enter the date to");
+        RuleFor(p => new { p.DateFrom, p.DateTo }).Must(p => p.DateFrom <= p.DateTo)
+            .WithMessage("Date to must be greater than date from");
+        RuleFor(p => p.Type)
+            .NotNull().WithMessage("You must enter the type");
+        RuleFor(p => p.Hours)
+            .Must(p => p > 0 && p < 1000)
+            .WithMessage("Hours should be at range (0-1000).");
+    }
+}
