efe49431ac0193e5dd27c7ad0039a3ceafb2cb95
kbroniek <karol.broniek.gtm@gmail.com>
2022-06-23 16:05:50 +0200

    Small refactor

---
+++ src/LeaveSystem.Api/LeaveSystem.Api/Endpoints/LeaveRequestEndpoints.cs
@@ -0,0 +1,26 @@
+﻿using GoldenEye.Commands;
+using Microsoft.Identity.Web.Resource;
+
+namespace LeaveSystem.Api.Endpoints;
+public static class LeaveRequestEndpoints
+{
+    public static void AddLeaveRequestEndpoints(this IEndpointRouteBuilder endpoint, string azureScpes)
+    {
+        endpoint.MapPost("api/createLeaveRequest", (HttpContext httpContext, ICommandBus commandBus, Web.Pages.CreatingLeaveRequest.CreateLeaveRequestDto leaveRequest) =>
+        {
+            httpContext.VerifyUserHasAnyAcceptedScope(azureScpes);
+            var command = EventSourcing.LeaveRequests.CreatingLeaveRequest.CreateLeaveRequest.Create(
+                        Guid.NewGuid(),
+                        leaveRequest.DateFrom,
+                        leaveRequest.DateTo,
+                        leaveRequest.Hours,
+                        leaveRequest.Type,
+                        leaveRequest.Remarks
+                    );
+            return commandBus.Send(command);
+
+        })
+        .WithName("LeaveRequest")
+        .RequireAuthorization();
+    }
+}
