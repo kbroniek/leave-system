096922d6d58ce60d4740733613ebcc643e64ec69
kbroniek <karol.broniek.gtm@gmail.com>
2022-09-02 11:35:34 +0200

    Fix flags

--- src/LeaveSystem/LeaveSystem/EventSourcing/LeaveRequests/LeaveRequestStatus.cs
+++ src/LeaveSystem/LeaveSystem/EventSourcing/LeaveRequests/LeaveRequestStatus.cs
@@ -5,7 +5,7 @@ public enum LeaveRequestStatus
 {
     Pending = 1 << 0,
     Approved = 1 << 1,
-    Canceled = 1 << 3,
-    Rejected = 1 << 4,
+    Canceled = 1 << 2,
+    Rejected = 1 << 3,
     Valid = Pending | Approved,
 }
