33186ac5d4b7ff8fe3c69d888659cd39c449b70d
kbroniek <karol.broniek.gtm@gmail.com>
2022-10-06 10:59:30 +0200

    Use leave type service in the web

--- src/LeaveSystem.Web/LeaveSystem.Web/PagedListResponse.cs
+++ src/LeaveSystem.Web/LeaveSystem.Web/PagedListResponse.cs
@@ -1,26 +1,3 @@
-using System.Text.Json.Serialization;
-
 namespace LeaveSystem.Web;
 
-public class PagedListResponse<T>
-{
-    public IEnumerable<T>? Items { get; set; }
-
-    public long TotalItemCount { get; set; }
-
-    public bool HasNextPage { get; set; }
-
-    //[JsonConstructor]
-    //public PagedListResponse()
-    //{
-    //    Items = new List<T>();
-    //    TotalItemCount = 0;
-    //    HasNextPage = false;
-    //}
-    //public PagedListResponse(IEnumerable<T> items, long totalItemCount, bool hasNextPage)
-    //{
-    //    Items = items.ToList();
-    //    TotalItemCount = totalItemCount;
-    //    HasNextPage = hasNextPage;
-    //}
-}
+public record class PagedListResponse<T>(IEnumerable<T>? Items, long TotalItemCount, bool HasNextPage);
--- src/LeaveSystem.Web/LeaveSystem.Web/Services/LeaveTypeService.cs
+++ src/LeaveSystem.Web/LeaveSystem.Web/Services/LeaveTypeService.cs
@@ -1,6 +1,27 @@
-﻿namespace LeaveSystem.Web.Services;
+﻿using System.Net.Http.Json;
 
-    public class LeaveTypeService
+namespace LeaveSystem.Web.Services;
+
+public class LeaveTypeService
+{
+    private readonly HttpClient httpClient;
+
+    public LeaveTypeService(HttpClient httpClient)
     {
+        this.httpClient = httpClient;
     }
+
+    public async Task<IEnumerable<LeaveTypeDto>> GetLeaveTypes()
+    {
+        var leaveTypes = await httpClient.GetFromJsonAsync<ODataResponse<IEnumerable<LeaveTypeDto>>>("odata/LeaveTypes?$select=Id,Name&$orderby=Order asc");
+        return leaveTypes?.Data ?? Enumerable.Empty<LeaveTypeDto>();
+    }
+
+
+    public class LeaveTypeDto
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
 
