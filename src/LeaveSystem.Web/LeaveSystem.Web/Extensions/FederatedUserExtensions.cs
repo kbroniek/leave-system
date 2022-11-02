4c2f1917fcd734117406bfdbcbf6f1ae673e1f70
kbroniek <karol.broniek.gtm@gmail.com>
2022-11-02 12:35:12 +0100

    Add extension for getting user's email

---
+++ src/LeaveSystem.Web/LeaveSystem.Web/Extensions/FederatedUserExtensions.cs
@@ -0,0 +1,11 @@
+﻿using LeaveSystem.Shared;
+
+namespace LeaveSystem.Web.Extensions;
+
+public static class FederatedUserExtensions
+{
+    public static string GetEmail(this FederatedUser user)
+    {
+        return user.Email.Substring(2, user.Email.Length - 4); // because we get the email in weird format i.e. ['example@test.com']
+    }
+}
