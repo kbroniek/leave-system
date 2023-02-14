70eb1a05ed19ca23c18b88b43c9174a66a547acf
kbroniek <karol.broniek.gtm@gmail.com>
2023-02-14 08:02:12 +0100

    Show user panel by year

---
+++ src/LeaveSystem.Web/LeaveSystem.Web/Pages/UserPanel/LeaveRequestForView.cs
@@ -0,0 +1,20 @@
+﻿using LeaveSystem.Shared.LeaveRequests;
+using LeaveSystem.Web.Extensions;
+using LeaveSystem.Web.Pages.LeaveRequests.ShowingLeaveRequests;
+using LeaveSystem.Web.Pages.LeaveTypes;
+
+namespace LeaveSystem.Web.Pages.UserPanel;
+
+public record class LeaveRequestForView(DateTimeOffset DateFrom, DateTimeOffset DateTo, string Duration, LeaveRequestStatus Status, string LeaveTypeName)
+{
+    public static LeaveRequestForView Create(LeaveRequestShortInfo leaveRequest, IEnumerable<LeaveTypesService.LeaveTypeDto> leaveTypes, TimeSpan workingHours)
+    {
+        return new LeaveRequestForView(
+            leaveRequest.DateFrom,
+            leaveRequest.DateTo,
+            leaveRequest.Duration.GetReadableTimeSpan(workingHours),
+            leaveRequest.Status,
+            leaveTypes.FirstOrDefault(lt => lt.Id == leaveRequest.LeaveTypeId)?.Name ?? leaveRequest.LeaveTypeId.ToString()
+        );
+    }
+}
