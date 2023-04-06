d4592bcdbc2c133a1f65a4dff070897fd1d0888e
broni <broniekf@gmail,com>
2023-04-06 14:37:12 +0200

    change configuration

---
+++ src/LeaveSystem/LeaveSystem/Migrations/20230406113826_InitialCreate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveSystem.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
