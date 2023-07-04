220a23882f436f09a6b66651d34db28f7139fb22
kbroniek <karol.broniek.gtm@gmail.com>
2023-07-04 10:27:00 +0200

    Fix build issues

--- src/LeaveSystem.Web/LeaveSystem.Web/Pages/UsersManagement/UsersDto.cs
+++ src/LeaveSystem.Web/LeaveSystem.Web/Pages/UsersManagement/UsersDto.cs
@@ -1,14 +1,20 @@
-﻿using LeaveSystem.Shared;
-
-namespace LeaveSystem.Web.Pages.LeaveRequests.CreatingLeaveRequest;
+﻿namespace LeaveSystem.Web.Pages.LeaveRequests.CreatingLeaveRequest;
 
 public record class UsersDto(IEnumerable<UserDto> Items);
 public class UserDto
 {
+    public UserDto(string? id, string? name, string? email, IEnumerable<string>? roles)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Roles = roles;
+    }
+
     public string? Id { get; set; }
     public string? Name { get; set; }
     public string? Email { get; set; }
     public IEnumerable<string>? Roles { get; set; }
     public static UserDto Create() =>
-        new UserDto();
-};
+        new(null, null, null, null);
+}
