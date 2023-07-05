c62bb3bd291553f9c09fe9d83a68777e8474e538
kbroniek <karol.broniek.gtm@gmail.com>
2023-07-05 09:33:42 +0200

    Simplify config

--- src/LeaveSystem.Api/LeaveSystem.Api/Auth/Config.cs
+++ src/LeaveSystem.Api/LeaveSystem.Api/Auth/Config.cs
@@ -28,81 +28,45 @@ public static class Config
         services.AddScoped<IAuthorizationHandler, RoleRequirementHandler>();
     }
 
-    private static IServiceCollection AddLeaveRequestsAuthorization(this IServiceCollection services)
-    {
+    private static IServiceCollection AddLeaveRequestsAuthorization(this IServiceCollection services) =>
         services
-            .AddAuthorization(options =>
-                options.AddPolicy(LeaveRequestsEndpoints.GetLeaveRequestsPolicyName,
-                policy => policy.Requirements.Add(new RoleRequirement(RoleType.Employee, RoleType.DecisionMaker, RoleType.HumanResource))))
-            .AddAuthorization(options =>
-                options.AddPolicy(LeaveRequestsEndpoints.GetLeaveRequestDetailsPolicyName,
-                policy => policy.Requirements.Add(new RoleRequirement(RoleType.Employee, RoleType.DecisionMaker))))
-            .AddAuthorization(options =>
-                options.AddPolicy(LeaveRequestsEndpoints.CreateLeaveRequestPolicyName,
-                policy => policy.Requirements.Add(new RoleRequirement(RoleType.Employee))))
-            .AddAuthorization(options =>
-                options.AddPolicy(LeaveRequestsEndpoints.CreateLeaveRequestOnBehalfPolicyName,
-                policy => policy.Requirements.Add(new RoleRequirement(RoleType.DecisionMaker))))
-            .AddAuthorization(options =>
-                options.AddPolicy(LeaveRequestsEndpoints.AcceptLeaveRequestPolicyName,
-                policy => policy.Requirements.Add(new RoleRequirement(RoleType.DecisionMaker))))
-            .AddAuthorization(options =>
-                options.AddPolicy(LeaveRequestsEndpoints.RejectLeaveRequestPolicyName,
-                policy => policy.Requirements.Add(new RoleRequirement(RoleType.DecisionMaker))))
-            .AddAuthorization(options =>
-                options.AddPolicy(LeaveRequestsEndpoints.CancelLeaveRequestPolicyName,
-                policy => policy.Requirements.Add(new RoleRequirement(RoleType.Employee))));
-        return services;
-    }
+            .AddAuthorizationPolicy(LeaveRequestsEndpoints.GetLeaveRequestsPolicyName, RoleType.Employee, RoleType.DecisionMaker, RoleType.HumanResource)
+            .AddAuthorizationPolicy(LeaveRequestsEndpoints.GetLeaveRequestDetailsPolicyName, RoleType.Employee, RoleType.DecisionMaker)
+            .AddAuthorizationPolicy(LeaveRequestsEndpoints.CreateLeaveRequestPolicyName, RoleType.Employee)
+            .AddAuthorizationPolicy(LeaveRequestsEndpoints.CreateLeaveRequestOnBehalfPolicyName, RoleType.DecisionMaker)
+            .AddAuthorizationPolicy(LeaveRequestsEndpoints.AcceptLeaveRequestPolicyName, RoleType.DecisionMaker)
+            .AddAuthorizationPolicy(LeaveRequestsEndpoints.RejectLeaveRequestPolicyName, RoleType.DecisionMaker)
+            .AddAuthorizationPolicy(LeaveRequestsEndpoints.CancelLeaveRequestPolicyName, RoleType.Employee);
 
-    private static IServiceCollection AddWorkingHoursAuthorization(this IServiceCollection services)
-    {
+    private static IServiceCollection AddWorkingHoursAuthorization(this IServiceCollection services) =>
         services
-            .AddAuthorization(options =>
-                options.AddPolicy(WorkingHoursEndpoints.GetWorkingHoursEndpointsPolicyName,
-                policy => policy.Requirements.Add(RoleRequirement.AuhtorizeAll)))
-            .AddAuthorization(options =>
-                options.AddPolicy(WorkingHoursEndpoints.GetUserWorkingHoursEndpointsPolicyName,
-                policy => policy.Requirements.Add(RoleRequirement.AuhtorizeAll)))
-            .AddAuthorization(options =>
-                options.AddPolicy(WorkingHoursEndpoints.GetUserWorkingHoursDurationEndpointsPolicyName,
-                policy => policy.Requirements.Add(RoleRequirement.AuhtorizeAll)));
-        return services;
-    }
-    private static IServiceCollection AddEmployeesAuthorization(this IServiceCollection services)
-    {
+            .AddAuthorizationPolicy(WorkingHoursEndpoints.GetWorkingHoursEndpointsPolicyName, RoleRequirement.AuhtorizeAll)
+            .AddAuthorizationPolicy(WorkingHoursEndpoints.GetUserWorkingHoursEndpointsPolicyName, RoleRequirement.AuhtorizeAll)
+            .AddAuthorizationPolicy(WorkingHoursEndpoints.GetUserWorkingHoursDurationEndpointsPolicyName, RoleRequirement.AuhtorizeAll);
+    private static IServiceCollection AddEmployeesAuthorization(this IServiceCollection services) =>
         services
-            .AddAuthorization(options =>
-                options.AddPolicy(EmployeesEndpoints.GetEmployeesPolicyName,
-                policy => policy.Requirements.Add(new RoleRequirement(RoleType.DecisionMaker))));
-        services
-            .AddAuthorization(options =>
-                options.AddPolicy(EmployeesEndpoints.GetEmployeePolicyName,
-                policy => policy.Requirements.Add(new RoleRequirement(RoleType.HumanResource))));
-        return services;
-    }
-    private static IServiceCollection AddUsersAuthorization(this IServiceCollection services)
-    {
+            .AddAuthorizationPolicy(EmployeesEndpoints.GetEmployeesPolicyName, RoleType.DecisionMaker)
+            .AddAuthorizationPolicy(EmployeesEndpoints.GetEmployeePolicyName, RoleType.HumanResource);
+    private static IServiceCollection AddUsersAuthorization(this IServiceCollection services) =>
         services
             .AddAuthorizationPolicy(UsersEndpoints.GetUsersPolicyName, RoleType.UserAdmin)
             .AddAuthorizationPolicy(UsersEndpoints.AddUserPolicyName, RoleType.UserAdmin)
             .AddAuthorizationPolicy(UsersEndpoints.UpdateUserPolicyName, RoleType.UserAdmin);
-        return services;
-    }
 
-    private static IServiceCollection AddAuthorizationPolicy(this IServiceCollection services, string policyName, RoleType role) =>
-        services
-            .AddAuthorization(options =>
-                options.AddPolicy(policyName,
-                policy => policy.Requirements.Add(new RoleRequirement(role))));
-
-    private static IServiceCollection AddRolesAuthorization(this IServiceCollection services)
-    {
+    private static IServiceCollection AddRolesAuthorization(this IServiceCollection services) =>
         services
             .AddAuthorization(options =>
                 options.AddPolicy(RolesEndpoints.GetRolesPolicyName,
                 policy => policy.Requirements.Add(new RoleRequirement(RoleType.UserAdmin))));
-        return services;
-    }
+
+    private static IServiceCollection AddAuthorizationPolicy(this IServiceCollection services, string policyName, params RoleType[] roles) =>
+        services
+            .AddAuthorizationPolicy(policyName, new RoleRequirement(roles));
+
+    private static IServiceCollection AddAuthorizationPolicy(this IServiceCollection services, string policyName, RoleRequirement roleRequirement) =>
+        services
+            .AddAuthorization(options =>
+                options.AddPolicy(policyName,
+                policy => policy.Requirements.Add(roleRequirement)));
 }
 
