671c35d45999691b30b324f17a28be50f32a20c9
broni <broniekf@gmail,com>
2023-07-07 11:27:39 +0200

    add HandleCreateLeaveRequestOnBehalfTest.cs

---
+++ src/LeaveSystem.UnitTests/EventSourcing/LeaveRequests/CreatingLeaveRequest/HandleCreateLeaveRequestOnBehalfTest.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using GoldenEye.Repositories;
+using LeaveSystem.EventSourcing.LeaveRequests;
+using LeaveSystem.EventSourcing.LeaveRequests.CreatingLeaveRequest;
+using LeaveSystem.Shared;
+using Moq;
+using Xunit;
+
+namespace LeaveSystem.UnitTests.EventSourcing.LeaveRequests.CreatingLeaveRequest;
+
+public class HandleCreateLeaveRequestOnBehalfTest
+{
+    private readonly Mock<IRepository<LeaveRequest>> repositoryMock = new();
+    private readonly Mock<LeaveRequestFactory> factoryMock = new(null, null, null);
+    private readonly CreateLeaveRequest command = CreateLeaveRequest.Create(
+        Guid.NewGuid(),
+        new DateTimeOffset(638242288542961190, TimeSpan.Zero),
+        new DateTimeOffset(638242288542961190, TimeSpan.Zero),
+        TimeSpan.FromHours(8),
+        Guid.NewGuid(),
+        "fake remarks",
+        FederatedUser.Create("fakeId", "fake@emial.com", "Philip")
+    );
+    
+    [Fact]
+    public async Task WhenCreateLeaveRequestHandled_Create()
+    {
+        //Given
+        var createdEvent = LeaveRequestCreated.Create(
+            command.LeaveRequestId,
+            command.DateFrom,
+            command.DateTo,
+            command.Duration!.Value,
+            command.LeaveTypeId,
+            command.Remarks,
+            command.CreatedBy
+        );
+        var leaveRequest = LeaveRequest.CreatePendingLeaveRequest(createdEvent);
+        factoryMock
+            .Setup(f => f.Create(command, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(leaveRequest);
+        var handleAcceptLeaveRequest = new HandleCreateLeaveRequest(repositoryMock.Object, factoryMock.Object);
+        //When
+        await handleAcceptLeaveRequest.Handle(command, CancellationToken.None);
+        //Then
+        factoryMock.Verify(x => 
+            x.Create(command, It.IsAny<CancellationToken>()), Times.Once);
+        repositoryMock.Verify(x => 
+            x.Add(leaveRequest, It.IsAny<CancellationToken>()));
+        repositoryMock.Verify(x => 
+            x.SaveChanges(It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
