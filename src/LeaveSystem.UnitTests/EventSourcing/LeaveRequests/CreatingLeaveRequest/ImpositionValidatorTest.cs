0a6c00191a212a54d67be6c7d5c1c9d5a34ae622
kbroniek <karol.broniek.gtm@gmail.com>
2023-07-10 11:57:09 +0200

    Fix tests

--- src/LeaveSystem.UnitTests/EventSourcing/LeaveRequests/CreatingLeaveRequest/ImpositionValidatorTest.cs
+++ src/LeaveSystem.UnitTests/EventSourcing/LeaveRequests/CreatingLeaveRequest/ImpositionValidatorTest.cs
@@ -1,21 +1,19 @@
+using FluentAssertions;
+using LeaveSystem.EventSourcing.LeaveRequests;
+using LeaveSystem.EventSourcing.LeaveRequests.CreatingLeaveRequest;
+using LeaveSystem.Services;
+using LeaveSystem.Shared;
+using Marten;
+using Marten.Events;
+using Marten.Linq;
+using Moq;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
-using FluentAssertions;
-using GoldenEye.Extensions.Basic;
-using GoldenEye.Marten.Events.Storage;
-using LeaveSystem.EventSourcing.LeaveRequests;
-using LeaveSystem.EventSourcing.LeaveRequests.CreatingLeaveRequest;
-using LeaveSystem.Services;
-using LeaveSystem.Shared;
-using Marten;
-using Marten.Linq;
-using Moq;
 using Xunit;
 
 namespace LeaveSystem.UnitTests.EventSourcing.LeaveRequests.CreatingLeaveRequest;
@@ -26,18 +24,13 @@ public class ImpositionValidatorTest
     public async Task WhenOtherLeveRequestOverlappingWithThisDate_ThenThrowValidationException()
     {
         //Given
-        var queryableMock = new MartenQueryableMock<LeaveRequestCreated>(
-            GetLeaveRequestCreatedEvents(),
-            new Mock<Expression>().Object,
-            new Mock<IQueryProvider>().Object
-        );
         var dbContext = await DbContextFactory.CreateDbContextAsync();
         var workingHoursServiceMock = new Mock<WorkingHoursService>();
         var documentSessionMock = new Mock<IDocumentSession>();
-        documentSessionMock.Setup(v => v.Events.QueryRawEventDataOnly<LeaveRequestCreated>())
-            .Returns(queryableMock);
-        var creatingLeaveRequest = LeaveRequestCreated.Create(
-            Guid.Empty,
+        var eventStoreMock = new Mock<IEventStore>();
+        Guid fakeLeaveRequestId = Guid.NewGuid();
+        LeaveRequestCreated fakeLeaveRequestCreatedEvent = LeaveRequestCreated.Create(
+            fakeLeaveRequestId,
             new DateTimeOffset(2023, 7, 11, 0, 0, 0, TimeSpan.FromHours(5)),
             new DateTimeOffset(2023, 7, 13, 0, 0, 0, TimeSpan.FromHours(5)),
             TimeSpan.FromDays(6),
@@ -45,12 +38,39 @@ public class ImpositionValidatorTest
             "fake remarks",
             FederatedUser.Create("1", "fakeUser@fake.com", "Fakeoslav")
         );
-        var leaveRequestCreatedEvents = GetLeaveRequestCreatedEvents().AsQueryable();
-        var leaveRequestsFromDb = GetLeaveRequestsFromDb();
+        var fakeLeaveRequestEntity = LeaveRequest.CreatePendingLeaveRequest(fakeLeaveRequestCreatedEvent);
+        var queryableMock = new MartenList<LeaveRequestCreated>() {
+            fakeLeaveRequestCreatedEvent
+        };
+        documentSessionMock.SetupGet(v => v.Events)
+            .Returns(eventStoreMock.Object);
+        eventStoreMock.Setup(v => v.QueryRawEventDataOnly<LeaveRequestCreated>())
+            .Returns(queryableMock);
+        eventStoreMock.Setup(v => v.AggregateStreamAsync<LeaveRequest>(
+                fakeLeaveRequestId,
+                It.IsAny<long>(),
+                It.IsAny<DateTimeOffset?>(),
+                It.IsAny<LeaveRequest>(),
+                It.IsAny<long>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(fakeLeaveRequestEntity)
+            .Verifiable();
+        var testingLeaveRequest = LeaveRequestCreated.Create(
+            Guid.NewGuid(),
+            new DateTimeOffset(2023, 7, 11, 0, 0, 0, TimeSpan.FromHours(5)),
+            new DateTimeOffset(2023, 7, 13, 0, 0, 0, TimeSpan.FromHours(5)),
+            TimeSpan.FromDays(6),
+            Guid.NewGuid(),
+            "fake remarks",
+            FederatedUser.Create("1", "fakeUser@fake.com", "Fakeoslav")
+        );
+
+        //var leaveRequestCreatedEvents = GetLeaveRequestCreatedEvents().AsQueryable();
+        //var leaveRequestsFromDb = GetLeaveRequestsFromDb();
         var requestValidator =
             new CreateLeaveRequestValidator(dbContext, workingHoursServiceMock.Object, documentSessionMock.Object);
         //When
-        var act = async () => { await requestValidator.ImpositionValidator(creatingLeaveRequest); };
+        var act = async () => { await requestValidator.ImpositionValidator(testingLeaveRequest); };
         //Then
         await act.Should().ThrowAsync<ValidationException>();
     }
@@ -165,44 +185,38 @@ public class ImpositionValidatorTest
     }
 }
 
-public class MartenQueryableMock<T> : IMartenQueryable<T>
+public class MartenList<T> : List<T>, IMartenQueryable<T>
 {
-    private readonly IEnumerable<T> data;
+    private Mock<IQueryProvider> queryProviderMock = new();
+    public Type ElementType => typeof(T);
 
-    public MartenQueryableMock(IEnumerable<T> data, Expression expression, IQueryProvider provider)
+    public Expression Expression => Expression.Constant(this);
+
+    public IQueryProvider Provider
     {
-        this.data = data;
-        ElementType = typeof(T);
-        Expression = expression;
-        Provider = provider;
+        get
+        {
+            queryProviderMock
+                .Setup(x => x.CreateQuery<T>(It.IsAny<Expression>()))
+                .Returns(this);
+            return queryProviderMock.Object;
+        }
     }
 
-    public IEnumerator<T> GetEnumerator()
-    {
-        return data.GetEnumerator();
-    }
+    public QueryStatistics Statistics => throw new NotImplementedException();
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
+    public Task<bool> AnyAsync(CancellationToken token) => Task.FromResult(this.Any());
 
-    public Task<IReadOnlyList<TResult>> ToListAsync<TResult>(CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<bool> AnyAsync(CancellationToken token)
+    public Task<double> AverageAsync(CancellationToken token)
     {
         throw new NotImplementedException();
     }
 
-    public Task<int> CountAsync(CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
+    public Task<int> CountAsync(CancellationToken token) => Task.FromResult(Count);
 
-    public Task<long> CountLongAsync(CancellationToken token)
+    public Task<long> CountLongAsync(CancellationToken token) => Task.FromResult((long)Count);
+
+    public QueryPlan Explain(FetchType fetchType = FetchType.FetchMany, Action<IConfigureExplainExpressions>? configureExplain = null)
     {
         throw new NotImplementedException();
     }
@@ -217,6 +231,33 @@ public class MartenQueryableMock<T> : IMartenQueryable<T>
         throw new NotImplementedException();
     }
 
+    public IMartenQueryable<T> Include<TInclude>(Expression<Func<T, object>> idSource, Action<TInclude> callback) where TInclude : notnull
+    {
+        throw new NotImplementedException();
+    }
+
+    public IMartenQueryable<T> Include<TInclude>(Expression<Func<T, object>> idSource, IList<TInclude> list) where TInclude : notnull
+    {
+        throw new NotImplementedException();
+    }
+
+    public IMartenQueryable<T> Include<TInclude, TKey>(Expression<Func<T, object>> idSource, IDictionary<TKey, TInclude> dictionary)
+        where TInclude : notnull
+        where TKey : notnull
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TResult> MaxAsync<TResult>(CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TResult> MinAsync<TResult>(CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
     public Task<TResult> SingleAsync<TResult>(CancellationToken token)
     {
         throw new NotImplementedException();
@@ -227,62 +268,21 @@ public class MartenQueryableMock<T> : IMartenQueryable<T>
         throw new NotImplementedException();
     }
 
-    public Task<TResult> SumAsync<TResult>(CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<TResult> MinAsync<TResult>(CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<TResult> MaxAsync<TResult>(CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<double> AverageAsync(CancellationToken token)
-    {
-        throw new NotImplementedException();
-    }
-
-    public QueryPlan Explain(FetchType fetchType = FetchType.FetchMany,
-        Action<IConfigureExplainExpressions>? configureExplain = null)
-    {
-        throw new NotImplementedException();
-    }
-
-    public QueryStatistics Statistics => throw new System.NotImplementedException();
-    public Type ElementType { get; }
-    public Expression Expression { get; }
-    public IQueryProvider Provider { get; }
-
-    public IMartenQueryable<T> Include<TInclude>(Expression<Func<T, object>> idSource, Action<TInclude> callback)
-        where TInclude : notnull
-    {
-        throw new NotImplementedException();
-    }
-
-    public IMartenQueryable<T> Include<TInclude>(Expression<Func<T, object>> idSource, IList<TInclude> list)
-        where TInclude : notnull
-    {
-        throw new NotImplementedException();
-    }
-
-    public IMartenQueryable<T> Include<TInclude, TKey>(Expression<Func<T, object>> idSource,
-        IDictionary<TKey, TInclude> dictionary) where TInclude : notnull where TKey : notnull
-    {
-        throw new NotImplementedException();
-    }
-
     public IMartenQueryable<T> Stats(out QueryStatistics stats)
     {
         throw new NotImplementedException();
     }
 
-    public IAsyncEnumerable<T> ToAsyncEnumerable(CancellationToken token = new CancellationToken())
+    public Task<TResult> SumAsync<TResult>(CancellationToken token)
     {
         throw new NotImplementedException();
     }
+
+    public IAsyncEnumerable<T> ToAsyncEnumerable(CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<TResult>> ToListAsync<TResult>(CancellationToken token) =>
+        Task.FromResult(this.ToList().AsReadOnly().As<IReadOnlyList<TResult>>());
 }
