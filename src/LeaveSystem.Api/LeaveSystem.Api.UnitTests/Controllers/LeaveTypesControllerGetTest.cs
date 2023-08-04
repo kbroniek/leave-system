281755148294a5355a35dda8e3c37b828ace0551
broni <broniekf@gmail,com>
2023-08-04 10:21:31 +0200

    add new project

---
+++ src/LeaveSystem.Api/LeaveSystem.Api.UnitTests/Controllers/LeaveTypesControllerGetTest.cs
@@ -0,0 +1,13 @@
+//using LeaveSystem.Api.Controllers;
+//using LeaveSystem.Db;
+//using LeaveSystem.Db.Entities;
+//
+//namespace LeaveSystem.Api.UnitTests.Controllers;
+//
+//public class LeaveTypesControllerGetTest : GenericCrudControllerGetTest<LeaveType, Guid>
+//{
+//    protected override GenericCrudController<LeaveType, Guid> GetSut(LeaveSystemDbContext dbContext)
+//    {
+//        return new LeaveTypesController(dbContext);
+//    }
+//}
