f235f932f65492ea1d2a96011c90f55ccae1efec
Karol Broniek <58724721+kbroniek@users.noreply.github.com>
2023-08-22 12:45:24 +0200

    Users management (#35)
    * Add styles for holidays
    
    * Show hours in the items timeline
    
    * Tooltip
    
    * Style leave types
    
    * Adjust styles
    
    * Create LeaveTypeCreator
    
    * Make styles for statuses
    
    * Make styles for statuses
    
    * Create settings table
    
    * Create controller
    
    * Save enum as string
    
    * Use service to get settings
    
    * Fix getting settings.
    
    * Change colors
    
    * Add subgroups
    
    * Change settings color
    
    * Cleanup file
    
    * Show users
    
    * Crud frontend
    
    * Fix build issues
    
    * Remove redundant file
    
    * Remove unused lines in the index.html
    
    * Add, update users api
    
    * Simplify config
    
    * Remove roles endpoint
    
    * Fix fill in Azure
    
    * Remove unused css
    
    * Fix add and update user in the Azure
    
    * Fix names
    
    * Small adjustment.

---
+++ src/LeaveSystem.Web/LeaveSystem.Web/Pages/UsersManagement/UsersDto.cs
@@ -0,0 +1,21 @@
+﻿namespace LeaveSystem.Web.Pages.LeaveRequests.CreatingLeaveRequest;
+
+public record class UsersDto(IEnumerable<UserDto> Items);
+public class UserDto
+{
+    public UserDto() { }
+    public UserDto(string? id, string? name, string? email, IEnumerable<string>? roles)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Roles = roles;
+    }
+
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public IEnumerable<string>? Roles { get; set; }
+    public static UserDto Create() =>
+        new();
+}
