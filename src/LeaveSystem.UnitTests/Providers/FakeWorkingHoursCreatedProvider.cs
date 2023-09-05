dd1c490262bcbe9f9fb3f1fbd19fc203cb67ee21
broni <broniekf@gmail,com>
2023-09-05 10:58:58 +0200

    Add ValidateWorkingHoursUniqueTest.cs

--- src/LeaveSystem.UnitTests/Providers/FakeWorkingHoursCreatedProvider.cs
+++ src/LeaveSystem.UnitTests/Providers/FakeWorkingHoursCreatedProvider.cs
@@ -1,13 +1,36 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using LeaveSystem.EventSourcing.WorkingHours;
 using LeaveSystem.EventSourcing.WorkingHours.CreatingWorkingHours;
 using LeaveSystem.Shared;
 
 namespace LeaveSystem.UnitTests.Providers;
 
-public class FakeWorkingHoursCreatedProvider
+public static class FakeWorkingHoursCreatedProvider
 {
-    public WorkingHoursCreated GetForBen() => WorkingHoursCreated.Create(
+    public static WorkingHoursCreated GetForBen() => WorkingHoursCreated.Create(
         Guid.NewGuid(), FakeUserProvider.BenId, DateTimeOffsetExtensions.CreateFromDate(2020, 12, 6),
-        DateTimeOffsetExtensions.CreateFromDate(2020, 12, 6), TimeSpan.FromHours(8)
+        DateTimeOffsetExtensions.CreateFromDate(2025, 12, 6), TimeSpan.FromHours(8)
         );
+
+    public static WorkingHoursCreated GetForPhilip() => WorkingHoursCreated.Create(
+        Guid.NewGuid(), FakeUserProvider.PhilipId, DateTimeOffsetExtensions.CreateFromDate(2021, 12, 6),
+        DateTimeOffsetExtensions.CreateFromDate(2023, 5, 1), TimeSpan.FromHours(8)
+        );
+    
+    public static WorkingHoursCreated GetForFakeoslav() => WorkingHoursCreated.Create(
+        Guid.NewGuid(), FakeUserProvider.FakseoslavId, DateTimeOffsetExtensions.CreateFromDate(2018, 6, 6),
+        DateTimeOffsetExtensions.CreateFromDate(2026, 5, 1), TimeSpan.FromHours(8)
+    );
+
+    public static IEnumerable<WorkingHoursCreated> GetAll(bool withBen = true)
+    {
+        yield return GetForPhilip();
+        yield return GetForFakeoslav();
+        if (withBen)
+        {
+            yield return GetForBen();
+        }
+    } 
 }
