69268ca8805cbf5878694307ff6bc0086b32df9c
kbroniek <karol.broniek.gtm@gmail.com>
2023-09-08 13:32:49 +0200

    Fix GraphClientFactoryCreateTest UTs

--- src/LeaveSystem.Api/LeaveSystem.Api.UnitTests/GraphApi/GraphClientFactoryCreateTest.cs
+++ src/LeaveSystem.Api/LeaveSystem.Api.UnitTests/GraphApi/GraphClientFactoryCreateTest.cs
@@ -1,4 +1,3 @@
-using FluentAssertions;
 using LeaveSystem.Api.GraphApi;
 
 namespace LeaveSystem.Api.UnitTests.GraphApi;
@@ -6,9 +5,9 @@ namespace LeaveSystem.Api.UnitTests.GraphApi;
 public class GraphClientFactoryCreateTest
 {
     [Theory]
-    [InlineData(null, "fakeClientId", "fakeSecret", new [] {"fakeScope1"})]
-    [InlineData("fakeTenantId", null, "fakeSecret", new [] {"fakeScope1"})]
-    [InlineData("fakeTenantId", "fakeClientId", null, new [] {"fakeScope1"})]
+    [InlineData(null, "fakeClientId", "fakeSecret", new[] { "fakeScope1" })]
+    [InlineData("fakeTenantId", null, "fakeSecret", new[] { "fakeScope1" })]
+    [InlineData("fakeTenantId", "fakeClientId", null, new[] { "fakeScope1" })]
     [InlineData("fakeTenantId", "fakeClientId", "fakeSecret", null)]
     public void WhenTenantIdOrClientIdOrSecretOrScopesIsNull_ThenThrowArgumentNullException(
         string? tenantId, string? clientId, string? secret, string[]? scopes)
@@ -21,12 +20,12 @@ public class GraphClientFactoryCreateTest
         //Then
         act.Should().Throw<ArgumentNullException>();
     }
-    
+
     [Theory]
-    [InlineData("  ", "fakeClientId", "fakeSecret", new [] {"fakeScope1"})]
-    [InlineData("fakeTenatId", "  ", "fakeSecret", new [] {"fakeScope1"})]
-    [InlineData("fakeTenatId", "fakeClientId", "  ", new [] {"fakeScope1"})]
-    [InlineData("fakeTenatId", "fakeClientId", "fakeSecret", new string[]{ })]
+    [InlineData("  ", "fakeClientId", "fakeSecret", new[] { "fakeScope1" })]
+    [InlineData("fakeTenatId", "  ", "fakeSecret", new[] { "fakeScope1" })]
+    [InlineData("fakeTenatId", "fakeClientId", "  ", new[] { "fakeScope1" })]
+    [InlineData("fakeTenatId", "fakeClientId", "fakeSecret", new string[] { })]
     public void WhenTenantIdOrClientIdOrSecretIsWhiteSpaceOrScopesIsEmpty_ThenThrowArgumentException(
         string? tenantId, string? clientId, string? secret, string[]? scopes)
     {
@@ -37,5 +36,20 @@ public class GraphClientFactoryCreateTest
         };
         //Then
         act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void WhenCreated_ThenGraphServiceClientHaveProperlyCreatedAttributes()
+    {
+        //Given
+        const string tenantId = "fakeTenatId";
+        const string clientId = "fakeClientId";
+        const string secret = "fakeSecret";
+        string[] scopes = { "fakeScp1", "fakeScp2" };
+        var factory = GraphClientFactory.Create(tenantId, clientId, secret, scopes);
+        //When
+        var client = factory.Create();
+        //Then
+        client.Should().NotBeNull();
     }
 }
