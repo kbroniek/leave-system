2d1ac1c70dd05fb5aba53e39950b1dfd89ae2469
kbroniek <karol.broniek.gtm@gmail.com>
2023-12-08 13:55:49 +0100

    Fix displaying working hours

--- src/LeaveSystem.Web/LeaveSystem.Web/Pages/UsersManagement/UserWithWorkingHoursDto.cs
+++ src/LeaveSystem.Web/LeaveSystem.Web/Pages/UsersManagement/UserWithWorkingHoursDto.cs
@@ -1,3 +1,4 @@
+using LeaveSystem.Shared;
 using LeaveSystem.Shared.WorkingHours;
 
 namespace LeaveSystem.Web.Pages.UsersManagement;
@@ -8,7 +9,7 @@ public class UserWithWorkingHoursDto : UserDto
 
     public UserWithWorkingHoursDto()
     {
-        
+        WorkingHours = new List<WorkingHoursDto>();
     }
 
     public UserWithWorkingHoursDto(string? id, string? name, string? email, IEnumerable<string>? roles, IEnumerable<WorkingHoursDto> workingHours) : base(id, name, email, roles)
@@ -17,4 +18,24 @@ public class UserWithWorkingHoursDto : UserDto
     }
 
     public UserDto GetUser() => new(Id, Name, Email, Roles);
+
+
+    public string TimeProxy
+    {
+        get => CurrentWorkingHours?.ToString() ?? "";
+        set
+        {
+            TimeSpan.TryParse(value, out var parsedValue);
+        }
+    }
+
+
+    public TimeSpan? CurrentWorkingHours
+    {
+        get
+        {
+            DateTimeOffset now = DateTimeOffset.Now.GetDayWithoutTime();
+            return WorkingHours?.FirstOrDefault(x => x.DateFrom <= now && (x.DateTo >= now || x.DateTo is null))?.Duration;
+        }
+    }
 }
