cf977aed6227e9726c784b28f8557589fe9cde9c
kbroniek <karol.broniek.gtm@gmail.com>
2023-12-15 15:24:44 +0100

    Add UTs for BasicValidator

--- src/LeaveSystem/LeaveSystem/EventSourcing/LeaveRequests/CreatingLeaveRequest/Validators/BasicValidator.cs
+++ src/LeaveSystem/LeaveSystem/EventSourcing/LeaveRequests/CreatingLeaveRequest/Validators/BasicValidator.cs
@@ -30,19 +30,20 @@ public class BasicValidator
     public virtual void Validate(LeaveRequestCreated @event, TimeSpan minDuration,
         TimeSpan maxDuration, bool? includeFreeDays)
     {
+        Guard.Against.NegativeOrZero(@event.Duration, nameof(@event.Duration));
         Guard.Against.OutOfRange(@event.Duration, nameof(@event.Duration), minDuration,
             maxDuration);
         if (includeFreeDays != false) return;
         var dateFromDayKind = DateCalculator.GetDayKind(@event.DateFrom);
         if (dateFromDayKind != DateCalculator.DayKind.WORKING)
         {
-            throw new ArgumentOutOfRangeException(nameof(@event.DateFrom), "The date is off work.");
+            throw new ArgumentOutOfRangeException(nameof(@event.DateFrom), "The date from is off work.");
         }
 
         var dateToDayKind = DateCalculator.GetDayKind(@event.DateTo);
         if (dateToDayKind != DateCalculator.DayKind.WORKING)
         {
-            throw new ArgumentOutOfRangeException(nameof(@event.DateTo), "The date is off work.");
+            throw new ArgumentOutOfRangeException(nameof(@event.DateTo), "The date to is off work.");
         }
     }
 }
