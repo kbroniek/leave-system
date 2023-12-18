007521db8108f760880691ea956ca1e2123976c0
kbroniek <karol.broniek.gtm@gmail.com>
2023-12-18 14:42:25 +0100

    Add one working hour to validator

--- src/LeaveSystem/LeaveSystem/EventSourcing/LeaveRequests/CreatingLeaveRequest/LeaveRequestFactory.cs
+++ src/LeaveSystem/LeaveSystem/EventSourcing/LeaveRequests/CreatingLeaveRequest/LeaveRequestFactory.cs
@@ -4,6 +4,7 @@ using LeaveSystem.Db.Entities;
 using LeaveSystem.EventSourcing.LeaveRequests.CreatingLeaveRequest.Validators;
 using LeaveSystem.Extensions;
 using LeaveSystem.Shared;
+using LeaveSystem.Shared.Date;
 using Marten;
 
 namespace LeaveSystem.EventSourcing.LeaveRequests.CreatingLeaveRequest;
@@ -13,26 +14,28 @@ public class LeaveRequestFactory
     private readonly CreateLeaveRequestValidator validator;
     private readonly LeaveSystemDbContext dbContext;
     private readonly IQuerySession querySession;
+    private readonly CurrentDateService dateService;
 
     public LeaveRequestFactory(CreateLeaveRequestValidator validator, LeaveSystemDbContext dbContext,
-        IQuerySession querySession)
+        IQuerySession querySession, CurrentDateService dateService)
     {
         this.validator = validator;
         this.dbContext = dbContext;
         this.querySession = querySession;
+        this.dateService = dateService;
     }
 
     public virtual async Task<LeaveRequest> Create(CreateLeaveRequest command, CancellationToken cancellationToken)
     {
         var leaveType = await GetLeaveType(command.LeaveTypeId);
-        var now = DateTimeOffset.Now.GetDayWithoutTime();
+        var now = dateService.GetWithoutTime();
         var workingHoursModel =
            await querySession.GetCurrentWorkingHoursForUser(command.CreatedBy.Id, now, cancellationToken)
            ?? throw new InvalidOperationException($"User with ID {command.CreatedBy.Id} does not have working Hours");
         var workingHours = workingHoursModel.Duration;
         var maxDuration = DateCalculator.CalculateDuration(command.DateFrom, command.DateTo, workingHours,
             leaveType.Properties?.IncludeFreeDays);
-        var minDuration = maxDuration - workingHours;
+        var minDuration = maxDuration - workingHours + TimeSpan.FromHours(1);
         var duration = command.Duration ?? maxDuration;
         var leaveRequestCreated = LeaveRequestCreated.Create(command.LeaveRequestId, command.DateFrom, command.DateTo,
             duration, command.LeaveTypeId, command.Remarks, command.CreatedBy, workingHours);
