f4bc921f4d74291c18494e7af9a9ba77c2062a44
kbroniek <karol.broniek.gtm@gmail.com>
2023-12-18 14:44:26 +0100

    Fix LeaveRequestFactoryTest

--- src/LeaveSystem/LeaveSystem.UnitTests/EventSourcing/LeaveRequests/CreatingLeaveRequest/LeaveRequestFactoryTest.cs
+++ src/LeaveSystem/LeaveSystem.UnitTests/EventSourcing/LeaveRequests/CreatingLeaveRequest/LeaveRequestFactoryTest.cs
@@ -1,20 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using FluentAssertions;
-using GoldenEye.Exceptions;
 using LeaveSystem.Db;
 using LeaveSystem.EventSourcing.LeaveRequests;
 using LeaveSystem.EventSourcing.LeaveRequests.CreatingLeaveRequest;
 using LeaveSystem.EventSourcing.LeaveRequests.CreatingLeaveRequest.Validators;
-using LeaveSystem.Shared.Date;
+using LeaveSystem.Shared;
 using LeaveSystem.Shared.LeaveRequests;
 using LeaveSystem.UnitTests.Providers;
 using LeaveSystem.UnitTests.Stubs;
 using Marten;
 using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace LeaveSystem.UnitTests.EventSourcing.LeaveRequests.CreatingLeaveRequest;
@@ -23,41 +21,31 @@ public class LeaveRequestFactoryTest
 {
     private static readonly TimeSpan WorkingHours = TimeSpan.FromHours(8);
 
-    private static LeaveRequestFactory GetSut(
-        LeaveSystemDbContext dbContext, IQuerySession querySession, CreateLeaveRequestValidator validator)
-    {
-        return new LeaveRequestFactory(validator, dbContext, querySession);
-    }
 
-    private static readonly DateTimeOffset Now = DateTimeOffset.Now;
+    private static readonly DateTimeOffset FakeNow = FakeDateServiceProvider.GetDateService().GetWithoutTime();
 
     [Fact]
     public async Task WhenLeaveTypeNotFound_ThenThrowNotFoundException()
     {
         //Given
         var fakeEvent = CreateLeaveRequest.Create(
-            Guid.NewGuid(),
-            new DateTimeOffset(2023, 7, 27, 0, 0, 0, TimeSpan.Zero),
-            new DateTimeOffset(2023, 8, 5, 0, 0, 0, TimeSpan.Zero),
-            WorkingHours * 3,
-            FakeLeaveTypeProvider.FakeSickLeaveId,
+            Guid.Parse("1c2782ae-1068-4a6d-bd64-7850a7aab0e1"),
+            DateTimeOffset.Parse("2023-12-16"),
+            DateTimeOffset.Parse("2023-12-16"),
+            WorkingHours,
+            Guid.Parse("1c2782ae-1068-4a6d-bd64-7850a7aab0e2"),
             "fake remarks",
             FakeUserProvider.GetUserWithNameFakeoslav()
         );
         await using var dbContext = await DbContextFactory.CreateDbContextAsync();
-        await dbContext.LeaveTypes.AddAsync(FakeLeaveTypeProvider.GetFakeOnDemandLeave());
         var querySessionMock = new Mock<IQuerySession>();
-        var martenQueryableStub = new MartenQueryableStub<LeaveSystem.EventSourcing.WorkingHours.WorkingHours>(
-            FakeWorkingHoursProvider.GetAll(Now)
-            );
-        querySessionMock.Setup(m => m.Query<LeaveSystem.EventSourcing.WorkingHours.WorkingHours>())
-            .Returns(martenQueryableStub);
         var validatorMock = GetValidatorMock();
         var sut = GetSut(dbContext, querySessionMock.Object, validatorMock.Object);
         //When
         var act = async () => { await sut.Create(fakeEvent, It.IsAny<CancellationToken>()); };
         //Then
-        await act.Should().ThrowAsync<Ardalis.GuardClauses.NotFoundException>();
+        var result = await act.Should().ThrowAsync<Ardalis.GuardClauses.NotFoundException>();
+        result.WithMessage("Queried object leaveTypeId was not found, Key: 1c2782ae-1068-4a6d-bd64-7850a7aab0e2");
         querySessionMock.Verify(x => x.Query<LeaveSystem.EventSourcing.WorkingHours.WorkingHours>(), Times.Never);
         validatorMock.Verify(x => x.Validate(
                 It.IsAny<LeaveRequestCreated>(), It.IsAny<TimeSpan>(), It.IsAny<TimeSpan>(), It.IsAny<bool?>()
@@ -65,36 +53,55 @@ public class LeaveRequestFactoryTest
         );
     }
 
-    private Mock<CreateLeaveRequestValidator> GetValidatorMock() =>
-        new(null, null, null);
-
     [Fact]
-    public async Task WhenDurationGreaterThanYear_ThenThrowArgumentOutOfRangeException()
+    public async Task WhenNoWorkingHoursForCreator_ThenThrowInvalidOperationException()
     {
-        //Given
         var fakeEvent = CreateLeaveRequest.Create(
-            Guid.NewGuid(),
-            new DateTimeOffset(2023, 7, 27, 0, 0, 0, TimeSpan.Zero),
-            new DateTimeOffset(2025, 8, 5, 0, 0, 0, TimeSpan.Zero),
-            WorkingHours * 3,
+            Guid.Parse("1c2782ae-1068-4a6d-bd64-7850a7aab0e3"),
+            DateTimeOffset.Parse("2023-12-16"),
+            DateTimeOffset.Parse("2023-12-16"),
+            WorkingHours,
             FakeLeaveTypeProvider.FakeOnDemandLeaveId,
             "fake remarks",
             FakeUserProvider.GetUserWithNameFakeoslav()
         );
-        await using var dbContext = await DbContextFactory.CreateDbContextAsync();
-        await dbContext.LeaveTypes.AddAsync(FakeLeaveTypeProvider.GetFakeOnDemandLeave());
-        var querySessionMock = new Mock<IQuerySession>();
-        var martenQueryableStub = new MartenQueryableStub<LeaveSystem.EventSourcing.WorkingHours.WorkingHours>(
-            FakeWorkingHoursProvider.GetAll(Now)
-        );
-        querySessionMock.Setup(m => m.Query<LeaveSystem.EventSourcing.WorkingHours.WorkingHours>())
-            .Returns(martenQueryableStub);
+        await using var dbContext = await GetDbContext();
+        var querySessionMock = GetQuerySessionMock(new[] { FakeWorkingHoursProvider.GetCurrentForBen(DateTimeOffset.Parse("2023-12-15")) });
         var validatorMock = GetValidatorMock();
         var sut = GetSut(dbContext, querySessionMock.Object, validatorMock.Object);
         //When
         var act = async () => { await sut.Create(fakeEvent, It.IsAny<CancellationToken>()); };
         //Then
-        await act.Should().ThrowAsync<ArgumentOutOfRangeException>();
+        var result = await act.Should().ThrowAsync<InvalidOperationException>();
+        result.WithMessage("User with ID 1 does not have working Hours");
+        querySessionMock.Verify(x => x.Query<LeaveSystem.EventSourcing.WorkingHours.WorkingHours>(), Times.Once);
+        validatorMock.Verify(x => x.Validate(
+                It.IsAny<LeaveRequestCreated>(), It.IsAny<TimeSpan>(), It.IsAny<TimeSpan>(), It.IsAny<bool?>()
+            ), Times.Never
+        );
+    }
+    [Fact]
+    public async Task WhenDurationGreaterThanYear_ThenThrowArgumentOutOfRangeException()
+    {
+        //Given
+        var fakeEvent = CreateLeaveRequest.Create(
+            Guid.Parse("1c2782ae-1068-4a6d-bd64-7850a7aab0e5"),
+            DateTimeOffset.Parse("2023-07-27"),
+            DateTimeOffset.Parse("2025-08-05"),
+            WorkingHours * 3,
+            FakeLeaveTypeProvider.FakeOnDemandLeaveId,
+            "fake remarks",
+            FakeUserProvider.GetUserWithNameFakeoslav()
+        );
+        await using var dbContext = await GetDbContext();
+        var querySessionMock = GetQuerySessionMock();
+        var validatorMock = GetValidatorMock();
+        var sut = GetSut(dbContext, querySessionMock.Object, validatorMock.Object);
+        //When
+        var act = async () => { await sut.Create(fakeEvent, It.IsAny<CancellationToken>()); };
+        //Then
+        var result = await act.Should().ThrowAsync<ArgumentOutOfRangeException>();
+        result.WithMessage("Specified argument was out of the range of valid values. (Parameter 'Max range reached calculating duration between dates from: 2023-07-27T00:00:00.0000000+00:00 to: 2025-08-05T00:00:00.0000000+02:00. The max duration is 366 days.')");
         querySessionMock.Verify(x => x.Query<LeaveSystem.EventSourcing.WorkingHours.WorkingHours>(), Times.Once);
         validatorMock.Verify(x => x.Validate(
                 It.IsAny<LeaveRequestCreated>(), It.IsAny<TimeSpan>(), It.IsAny<TimeSpan>(), It.IsAny<bool?>()
@@ -106,9 +113,9 @@ public class LeaveRequestFactoryTest
     public async Task WhenCommandValid_ThenCreateLeaveRequest()
     {
         var fakeEvent = CreateLeaveRequest.Create(
-            Guid.NewGuid(),
-            new DateTimeOffset(2023, 7, 27, 0, 0, 0, TimeSpan.Zero),
-            new DateTimeOffset(2023, 8, 5, 0, 0, 0, TimeSpan.Zero),
+            Guid.Parse("1c2782ae-1068-4a6d-bd64-7850a7aab0e6"),
+            DateTimeOffset.Parse("2023-07-27"),
+            DateTimeOffset.Parse("2023-08-05"),
             WorkingHours * 3,
             FakeLeaveTypeProvider.FakeOnDemandLeaveId,
             "fake remarks",
@@ -116,12 +123,7 @@ public class LeaveRequestFactoryTest
         );
         await using var dbContext = await DbContextFactory.CreateDbContextAsync();
         await dbContext.LeaveTypes.AddAsync(FakeLeaveTypeProvider.GetFakeOnDemandLeave());
-        var querySessionMock = new Mock<IQuerySession>();
-        var martenQueryableStub = new MartenQueryableStub<LeaveSystem.EventSourcing.WorkingHours.WorkingHours>(
-            FakeWorkingHoursProvider.GetAll(Now)
-        );
-        querySessionMock.Setup(m => m.Query<LeaveSystem.EventSourcing.WorkingHours.WorkingHours>())
-            .Returns(martenQueryableStub);
+        var querySessionMock = GetQuerySessionMock();
         var validatorMock = GetValidatorMock();
         var sut = GetSut(dbContext, querySessionMock.Object, validatorMock.Object);
         //When
@@ -135,48 +137,40 @@ public class LeaveRequestFactoryTest
         leaveRequest.Should().BeEquivalentTo(new
         {
             Id = fakeEvent.LeaveRequestId,
-            DateFrom = fakeEvent.DateFrom,
-            DateTo = fakeEvent.DateTo,
+            DateFrom = fakeEvent.DateFrom.GetDayWithoutTime(),
+            DateTo = fakeEvent.DateTo.GetDayWithoutTime(),
             Duration = fakeEvent.Duration,
             LeaveTypeId = fakeEvent.LeaveTypeId,
-            Remarks = new List<LeaveRequest.RemarksModel>() {new (fakeEvent.Remarks!, fakeEvent.CreatedBy)},
+            Remarks = new List<LeaveRequest.RemarksModel>() { new(fakeEvent.Remarks!, fakeEvent.CreatedBy) },
             Status = LeaveRequestStatus.Pending,
             CreatedBy = fakeEvent.CreatedBy,
             LastModifiedBy = fakeEvent.CreatedBy,
-            
-        },o => o.ExcludingMissingMembers());
+
+        }, o => o.ExcludingMissingMembers());
     }
 
-    [Fact]
-    public async Task WhenNoWorkingHoursForCreator_ThenThrowInvalidOperationException()
+    private static LeaveRequestFactory GetSut(
+        LeaveSystemDbContext dbContext, IQuerySession querySession, CreateLeaveRequestValidator validator)
     {
-                var fakeEvent = CreateLeaveRequest.Create(
-            Guid.NewGuid(),
-            new DateTimeOffset(2023, 7, 27, 0, 0, 0, TimeSpan.Zero),
-            new DateTimeOffset(2023, 8, 5, 0, 0, 0, TimeSpan.Zero),
-            WorkingHours * 3,
-            FakeLeaveTypeProvider.FakeOnDemandLeaveId,
-            "fake remarks",
-            FakeUserProvider.GetUserWithNameFakeoslav()
-        );
-        await using var dbContext = await DbContextFactory.CreateDbContextAsync();
-        await dbContext.LeaveTypes.AddAsync(FakeLeaveTypeProvider.GetFakeOnDemandLeave());
+        return new LeaveRequestFactory(validator, dbContext, querySession, FakeDateServiceProvider.GetDateService());
+    }
+    private static Mock<CreateLeaveRequestValidator> GetValidatorMock() => new(null, null, null);
+
+    private static Mock<IQuerySession> GetQuerySessionMock(IEnumerable<LeaveSystem.EventSourcing.WorkingHours.WorkingHours>? workingHours = null)
+    {
         var querySessionMock = new Mock<IQuerySession>();
         var martenQueryableStub = new MartenQueryableStub<LeaveSystem.EventSourcing.WorkingHours.WorkingHours>(
-            new [] {FakeWorkingHoursProvider.GetCurrentForBen(DateTimeOffset.Now)}
+            workingHours ?? FakeWorkingHoursProvider.GetAll(FakeNow)
         );
         querySessionMock.Setup(m => m.Query<LeaveSystem.EventSourcing.WorkingHours.WorkingHours>())
             .Returns(martenQueryableStub);
-        var validatorMock = GetValidatorMock();
-        var sut = GetSut(dbContext, querySessionMock.Object, validatorMock.Object);
-        //When
-        var act = async () => { await sut.Create(fakeEvent, It.IsAny<CancellationToken>()); };
-        //Then
-        await act.Should().ThrowAsync<InvalidOperationException>();
-        querySessionMock.Verify(x => x.Query<LeaveSystem.EventSourcing.WorkingHours.WorkingHours>());
-        validatorMock.Verify(x => x.Validate(
-                It.IsAny<LeaveRequestCreated>(), It.IsAny<TimeSpan>(), It.IsAny<TimeSpan>(), It.IsAny<bool?>()
-            ), Times.Never
-        );
+        return querySessionMock;
+    }
+
+    private static async Task<LeaveSystemDbContext> GetDbContext()
+    {
+        var dbContext = await DbContextFactory.CreateDbContextAsync();
+        await dbContext.LeaveTypes.AddAsync(FakeLeaveTypeProvider.GetFakeOnDemandLeave());
+        return dbContext;
     }
 }
