c379b487d94130747e5a829ca07adccc5f87bf57
kbroniek <karol.broniek.gtm@gmail.com>
2023-12-19 14:05:52 +0100

    Fix CreateLeaveRequestCreatedTest

--- src/LeaveSystem/LeaveSystem.UnitTests/EventSourcing/LeaveRequests/CreatingLeaveRequest/CreateLeaveRequestCreatedTest.cs
+++ src/LeaveSystem/LeaveSystem.UnitTests/EventSourcing/LeaveRequests/CreatingLeaveRequest/CreateLeaveRequestCreatedTest.cs
@@ -1,18 +1,14 @@
-using System;
-using System.Collections.Generic;
 using FluentAssertions;
 using LeaveSystem.EventSourcing.LeaveRequests.CreatingLeaveRequest;
 using LeaveSystem.Shared;
-using LeaveSystem.Shared.WorkingHours;
+using System;
 using Xunit;
 
 namespace LeaveSystem.UnitTests.EventSourcing.LeaveRequests.CreatingLeaveRequest;
 
 public class CreateLeaveRequestCreatedTest
 {
-    private static readonly TimeSpan WorkingHours = TimeSpan.FromHours(8);
-
-[Fact]
+    [Fact]
     public void WhenEmailIsInvalid_ThenThrowArgumentException()
     {
         //Given
@@ -21,20 +17,20 @@ public class CreateLeaveRequestCreatedTest
         var act = () =>
         {
             LeaveRequestCreated.Create(
-                Guid.NewGuid(),
-                new DateTimeOffset(2023, 7, 27, 0, 0, 0, TimeSpan.Zero),
-                new DateTimeOffset(2023, 7, 30, 0, 0, 0, TimeSpan.Zero),
-                WorkingHours * 3,
-                Guid.NewGuid(),
+                Guid.Parse("0e7e8dc7-d5b7-4e79-8897-10029c410f70"),
+                DateTimeOffset.Parse("2023-12-19"),
+                DateTimeOffset.Parse("2023-12-19"),
+                TimeSpan.FromHours(8),
+                Guid.Parse("0e7e8dc7-d5b7-4e79-8897-10029c410f71"),
                 "fake remarks",
                 fakeUser,
-                WorkingHoursUtils.DefaultWorkingHours
-                );
+                TimeSpan.FromHours(8)
+            );
         };
         //Then
-        act.Should().Throw<ArgumentException>();
+        act.Should().Throw<ArgumentException>().WithMessage("Input createdBy.Email was not in required format (Parameter 'createdBy.Email')");
     }
-    //Todo: Finish tests for this Method
+
     [Fact]
     public void WhenEmailIsNull_ThenThrowArgumentNullException()
     {
@@ -44,81 +40,78 @@ public class CreateLeaveRequestCreatedTest
         var act = () =>
         {
             LeaveRequestCreated.Create(
-                Guid.NewGuid(),
-                new DateTimeOffset(2023, 7, 27, 0, 0, 0, TimeSpan.Zero),
-                new DateTimeOffset(2023, 7, 30, 0, 0, 0, TimeSpan.Zero),
-                WorkingHours * 3,
-                Guid.NewGuid(),
+                Guid.Parse("0e7e8dc7-d5b7-4e79-8897-10029c410f72"),
+                DateTimeOffset.Parse("2023-12-20"),
+                DateTimeOffset.Parse("2023-12-20"),
+                TimeSpan.FromHours(8),
+                Guid.Parse("0e7e8dc7-d5b7-4e79-8897-10029c410f73"),
                 "fake remarks",
                 fakeUser,
-                WorkingHoursUtils.DefaultWorkingHours
-                );
+                TimeSpan.FromHours(8)
+            );
         };
         //Then
-        act.Should().Throw<ArgumentNullException>();
+        act.Should().Throw<ArgumentException>().WithMessage("Value cannot be null. (Parameter 'createdBy.Email')");
     }
-    // [Theory]
-    // [MemberData(nameof(GetDateOutOfYearTestData))]
-    // public void WhenDateIsOutOfYear_ThenThrowArgumentOutOfRangeException(DateTimeOffset dateFrom, DateTimeOffset dateTo)
-    // {
-    //     //Given
-    //     var fakeUser = FederatedUser.Create("1", "good.email@fake.com", "John");
-    //     //When
-    //     var act = () =>
-    //     {
-    //         LeaveRequestCreated.Create(
-    //             Guid.NewGuid(),
-    //             dateFrom,
-    //             dateTo,
-    //             WorkingHours * 3,
-    //             Guid.NewGuid(),
-    //             "fake remarks",
-    //             fakeUser,
-    //             WorkingHoursUtils.DefaultWorkingHours
-    //             );
-    //     };
-    //     //Then
-    //     act.Should().Throw<ArgumentOutOfRangeException>();
-    // }
-    //
-    // public static IEnumerable<object[]> GetDateOutOfYearTestData()
-    // {
-    //     yield return new object[]
-    //     {
-    //         new DateTimeOffset(2022, 12, 31, 0, 0, 0, TimeSpan.Zero),
-    //         new DateTimeOffset(2023, 1, 1, 0, 0, 0, TimeSpan.Zero),
-    //     };
-    //     yield return new object[]
-    //     {
-    //         new DateTimeOffset(2023, 12, 31, 0, 0, 0, TimeSpan.Zero),
-    //         new DateTimeOffset(2024, 1, 1, 0, 0, 0, TimeSpan.Zero),
-    //     };
-    //     yield return new object[]
-    //     {
-    //         new DateTimeOffset(2022, 12, 31, 0, 0, 0, TimeSpan.Zero),
-    //         new DateTimeOffset(2024, 1, 1, 0, 0, 0, TimeSpan.Zero),
-    //     };
-    // }
-    // [Fact]
-    // public void WhenDateFromIsGreaterThanDateTo_ThenThrowArgumentOutOfRangeException()
-    // {
-    //     //Given
-    //     var fakeUser = FederatedUser.Create("1", "good.email@fake.com", "John");
-    //     //When
-    //     var act = () =>
-    //     {
-    //         LeaveRequestCreated.Create(
-    //             Guid.NewGuid(),
-    //             new DateTimeOffset(2023, 12, 31, 0, 0, 0, TimeSpan.Zero),
-    //             new DateTimeOffset(2023, 10, 1, 0, 0, 0, TimeSpan.Zero),
-    //             WorkingHours * 3,
-    //             Guid.NewGuid(),
-    //             "fake remarks",
-    //             fakeUser,
-    //             WorkingHoursUtils.DefaultWorkingHours
-    //             );
-    //     };
-    //     //Then
-    //     act.Should().Throw<ArgumentOutOfRangeException>().WithMessage("*Date from has to be less than date to.*");
-    // }
+
+    [Theory]
+    [InlineData(-60)] // -1h
+    [InlineData(0)] // 0h
+    [InlineData(59)] // 59min
+    [InlineData(24 * 60 + 1)] //1d 1m
+    [InlineData(48 * 60)] // 2d
+    public void WhenWorkingHoursIsInvalid_ThenThrowArgumentOutOfRangeException(int workingHoursInMinutes)
+    {
+        //Given
+        var workingHours = TimeSpan.FromMinutes(workingHoursInMinutes);
+        //When
+        var act = () =>
+        {
+            LeaveRequestCreated.Create(
+                Guid.Parse("0e7e8dc7-d5b7-4e79-8897-10029c410f74"),
+                DateTimeOffset.Parse("2023-12-21"),
+                DateTimeOffset.Parse("2023-12-21"),
+                TimeSpan.FromHours(8),
+                Guid.Parse("0e7e8dc7-d5b7-4e79-8897-10029c410f75"),
+                "fake remarks",
+                FederatedUser.Create("1", "good.email@fake.com", "John"),
+                workingHours
+            );
+        };
+        //Then
+        act.Should().Throw<ArgumentOutOfRangeException>().WithMessage("Input workingHours was out of range (Parameter 'workingHours')");
+    }
+
+    [Theory]
+    [InlineData(60)] // 1h
+    [InlineData(8 * 60)] //8h
+    [InlineData(24 * 60)] // 1h
+    public void WhenWorkingHoursIsValid_ThenCreateObject(int workingHoursInMinutes)
+    {
+        //Given
+        var workingHours = TimeSpan.FromMinutes(workingHoursInMinutes);
+        var createdBy = FederatedUser.Create("1", "good.email@fake.com", "John");
+
+        //When
+        var result = LeaveRequestCreated.Create(
+            Guid.Parse("0e7e8dc7-d5b7-4e79-8897-10029c410f76"),
+            DateTimeOffset.Parse("2023-12-22T12:57:06.5608725+00:00"),
+            DateTimeOffset.Parse("2023-12-23T12:57:06.5608725+00:00"),
+            TimeSpan.FromHours(8),
+            Guid.Parse("0e7e8dc7-d5b7-4e79-8897-10029c410f77"),
+            "fake remarks",
+            createdBy,
+            workingHours
+        );
+
+        //Then
+        result.CreatedBy.Should().Be(createdBy);
+        result.DateFrom.Should().Be(DateTimeOffset.Parse("2023-12-22T00:00:00.00+00:00"));
+        result.DateTo.Should().Be(DateTimeOffset.Parse("2023-12-23T00:00:00.00+00:00"));
+        result.Duration.Should().Be(TimeSpan.FromHours(8));
+        result.LeaveRequestId.Should().Be(Guid.Parse("0e7e8dc7-d5b7-4e79-8897-10029c410f76"));
+        result.LeaveTypeId.Should().Be(Guid.Parse("0e7e8dc7-d5b7-4e79-8897-10029c410f77"));
+        result.Remarks.Should().Be("fake remarks");
+        result.WorkingHours.Should().Be(workingHours);
+    }
 }
