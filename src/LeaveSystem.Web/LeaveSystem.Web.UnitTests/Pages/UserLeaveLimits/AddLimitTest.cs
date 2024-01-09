09b54260a345b7767ca344fdd5085ffe5a933131
broni <broniekf@gmail,com>
2024-01-09 11:57:40 +0100

    Add get limits by used id test

--- src/LeaveSystem.Web/LeaveSystem.Web.UnitTests/Pages/UserLeaveLimits/AddLimitTest.cs
+++ src/LeaveSystem.Web/LeaveSystem.Web.UnitTests/Pages/UserLeaveLimits/AddLimitTest.cs
@@ -1,24 +1,13 @@
-using System.Net;
-using System.Text;
+namespace LeaveSystem.Web.UnitTests.Pages.UserLeaveLimits;
+
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using Blazored.Toast.Services;
-using LeaveSystem.Shared;
 using LeaveSystem.Shared.Converters;
 using LeaveSystem.Shared.UserLeaveLimits;
-using LeaveSystem.UnitTests.Providers;
-using LeaveSystem.Web.Pages.UserLeaveLimits;
-using LeaveSystem.Web.Pages.WorkingHours;
-using LeaveSystem.Web.Shared;
-using LeaveSystem.Web.UnitTests.TestStuff.Converters;
-using LeaveSystem.Web.UnitTests.TestStuff.Extensions;
-using LeaveSystem.Web.UnitTests.TestStuff.Factories;
-using LeaveSystem.Web.UnitTests.TestStuff.Providers;
 using Microsoft.Extensions.Logging;
 using Moq;
-using RichardSzalay.MockHttp;
-
-namespace LeaveSystem.Web.UnitTests.Pages.UserLeaveLimits;
+using Web.Pages.UserLeaveLimits;
+using Web.Shared;
 
 public class AddLimitTest
 {
@@ -26,7 +15,7 @@ public class AddLimitTest
     public async Task WhenEntityAdded_ThenReturnResultDto()
     {
         var universalHttpServiceMock = GetUniversalHttpServiceMock();
-        var fakeEntityToAdd = new AddUserLeaveLimitDto()
+        var fakeEntityToAdd = new AddUserLeaveLimitDto
         {
             AssignedToUserId = "fakeuserid",
             LeaveTypeId = Guid.Parse("54d432e0-68f2-4819-82e1-33107f8dd1de"),
@@ -34,12 +23,9 @@ public class AddLimitTest
             ValidUntil = DateTimeOffset.Parse("2023-04-21"),
             Limit = TimeSpan.FromHours(8),
             OverdueLimit = TimeSpan.FromHours(2),
-            Property = new AddUserLeaveLimitPropertiesDto()
-            {
-                Description = "fake description"
-            }
+            Property = new AddUserLeaveLimitPropertiesDto { Description = "fake description" }
         };
-        var expectedResult = new UserLeaveLimitsService.UserLeaveLimitDtoODataResponse()
+        var expectedResult = new UserLeaveLimitsService.UserLeaveLimitDtoODataResponse
         {
             Id = Guid.Parse("14497429-e3c7-428c-933f-5b69c28a73eb"),
             LeaveTypeId = Guid.Parse("54d432e0-68f2-4819-82e1-33107f8dd1de"),
@@ -47,17 +33,14 @@ public class AddLimitTest
             ValidUntil = DateTimeOffset.Parse("2023-04-21"),
             Limit = TimeSpan.FromHours(8),
             OverdueLimit = TimeSpan.FromHours(2),
-            Property = new()
-            {
-                Description = "fake description"
-            }
+            Property = new UserLeaveLimitPropertyDto { Description = "fake description" }
         };
         universalHttpServiceMock.Setup(m =>
-            m.AddAsync<AddUserLeaveLimitDto, UserLeaveLimitsService.UserLeaveLimitDtoODataResponse>(
-                "odata/UserLeaveLimits", 
-                It.Is<AddUserLeaveLimitDto>(d => IsDtoEquivalentTo(d, fakeEntityToAdd)), 
-                It.IsAny<string>(),
-                It.IsAny<JsonSerializerOptions>()))
+                m.AddAsync<AddUserLeaveLimitDto, UserLeaveLimitsService.UserLeaveLimitDtoODataResponse>(
+                    "odata/UserLeaveLimits",
+                    It.Is<AddUserLeaveLimitDto>(d => IsDtoEquivalentTo(d, fakeEntityToAdd)),
+                    It.IsAny<string>(),
+                    It.IsAny<JsonSerializerOptions>()))
             .ReturnsAsync(expectedResult);
         var sut = new UserLeaveLimitsService(universalHttpServiceMock.Object);
 
@@ -71,17 +54,15 @@ public class AddLimitTest
                 ValidUntil = DateTimeOffset.Parse("2023-04-21"),
                 Limit = TimeSpan.FromHours(8),
                 OverdueLimit = TimeSpan.FromHours(2),
-                Property = new
-                {
-                    Description = "fake description"
-                }
+                Property = new { Description = "fake description" }
             });
         universalHttpServiceMock.Verify(
             m => m.AddAsync<AddUserLeaveLimitDto, UserLeaveLimitsService.UserLeaveLimitDtoODataResponse>(
-                "odata/UserLeaveLimits", 
+                "odata/UserLeaveLimits",
                 It.Is<AddUserLeaveLimitDto>(d => IsDtoEquivalentTo(d, fakeEntityToAdd)),
-                "User leave limit added successfully", 
-                It.Is<JsonSerializerOptions>(o => o.Converters.Any(c => c.GetType() == typeof(TimeSpanIso8601Converter)))));
+                "User leave limit added successfully",
+                It.Is<JsonSerializerOptions>(
+                    o => o.Converters.Any(c => c.GetType() == typeof(TimeSpanIso8601Converter)))));
     }
 
     private static bool IsDtoEquivalentTo(AddUserLeaveLimitDto firstDto, AddUserLeaveLimitDto secondDto) =>
@@ -92,6 +73,7 @@ public class AddLimitTest
         firstDto.LeaveTypeId == secondDto.LeaveTypeId &&
         firstDto.AssignedToUserId == secondDto.AssignedToUserId;
 
-    private Mock<UniversalHttpService> GetUniversalHttpServiceMock() =>
-        new(new Mock<HttpClient>().Object, new Mock<IToastService>().Object, new Mock<ILogger<UniversalHttpService>>().Object);
+    private static Mock<UniversalHttpService> GetUniversalHttpServiceMock() =>
+        new(new Mock<HttpClient>().Object, new Mock<IToastService>().Object,
+            new Mock<ILogger<UniversalHttpService>>().Object);
 }
--- src/LeaveSystem.Web/LeaveSystem.Web/Pages/UserLeaveLimits/UserLeaveLimitsService.cs
+++ src/LeaveSystem.Web/LeaveSystem.Web/Pages/UserLeaveLimits/UserLeaveLimitsService.cs
@@ -1,72 +1,71 @@
-﻿using System.Net.Http.Json;
-using System.Text;
+﻿namespace LeaveSystem.Web.Pages.UserLeaveLimits;
+
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Xml;
-using Blazored.Toast.Services;
 using LeaveSystem.Shared.Converters;
 using LeaveSystem.Shared.UserLeaveLimits;
-using LeaveSystem.Web.Pages.WorkingHours;
-using LeaveSystem.Web.Shared;
-
-namespace LeaveSystem.Web.Pages.UserLeaveLimits;
+using Shared;
 
 public class UserLeaveLimitsService
 {
-    private readonly UniversalHttpService universalHttpService;
-    
-    private static readonly JsonSerializerOptions jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new(JsonSerializerDefaults.Web)
     {
-        Converters =
-            {
-                new TimeSpanIso8601Converter()
-            },
+        Converters = { new TimeSpanIso8601Converter() }
     };
 
-    public UserLeaveLimitsService(UniversalHttpService universalHttpService)
-    {
-        this.universalHttpService = universalHttpService;
-    }
+    private readonly UniversalHttpService universalHttpService;
 
-    public virtual async Task<IEnumerable<UserLeaveLimitDto>> GetLimits(string userId, DateTimeOffset since, DateTimeOffset until)
+    public UserLeaveLimitsService(UniversalHttpService universalHttpService) =>
+        this.universalHttpService = universalHttpService;
+
+    public virtual async Task<IEnumerable<UserLeaveLimitDto>> GetLimitsAsync(string userId, DateTimeOffset since,
+        DateTimeOffset until)
     {
-        var limits = await universalHttpService.GetAsync<ODataResponse<IEnumerable<UserLeaveLimitDto>>>(
+        var limits = await this.universalHttpService.GetAsync<ODataResponse<IEnumerable<UserLeaveLimitDto>>>(
             $"odata/UserLeaveLimits?$select=Id,Limit,OverdueLimit,LeaveTypeId,ValidSince,ValidUntil,Property&$filter=AssignedToUserId eq '{userId}' and ((ValidSince ge {since:s}Z or ValidSince eq null) and (ValidUntil le {until:s}.999Z or ValidUntil eq null))"
-            , "Can't get user leave limit", jsonSerializerOptions);
+            , "Can't get user leave limit", JsonSerializerOptions);
         return limits?.Data ?? Enumerable.Empty<UserLeaveLimitDto>();
     }
 
     public virtual async Task<UserLeaveLimitDto?> GetSingleAsync(Guid id)
     {
-        var odataResponse = await universalHttpService.GetAsync<UserLeaveLimitDtoODataResponse>(
+        var odataResponse = await this.universalHttpService.GetAsync<UserLeaveLimitDtoODataResponse>(
             $"odata/UserLeaveLimits({id})?$select=Id,Limit,OverdueLimit,LeaveTypeId,ValidSince,ValidUntil,Property",
-            "Can't get user leave limit", jsonSerializerOptions);
+            "Can't get user leave limit", JsonSerializerOptions);
         return odataResponse?.ToUserLeaveLimitDto();
     }
 
-    public virtual async Task<IEnumerable<LeaveLimitDto>> GetLimits(DateTimeOffset since, DateTimeOffset until)
+    public virtual async Task<IEnumerable<LeaveLimitDto>> GetLimitsAsync(DateTimeOffset since, DateTimeOffset until)
     {
-        var limits = await universalHttpService.GetAsync<ODataResponse<IEnumerable<LeaveLimitDto>>>(
+        var limits = await this.universalHttpService.GetAsync<ODataResponse<IEnumerable<LeaveLimitDto>>>(
             $"odata/UserLeaveLimits?$select=Id,Limit,OverdueLimit,LeaveTypeId,ValidSince,ValidUntil,Property,AssignedToUserId&$filter=not(AssignedToUserId eq null) and ((ValidSince ge {since:s}Z or ValidSince eq null) and (ValidUntil le {until:s}.999Z or ValidUntil eq null))"
-            , "Can't get user leave limit", jsonSerializerOptions);
+            , "Can't get user leave limit", JsonSerializerOptions);
         return limits?.Data ?? Enumerable.Empty<LeaveLimitDto>();
     }
 
     public async Task<UserLeaveLimitDto?> AddAsync(AddUserLeaveLimitDto entityToAdd)
     {
-        var odataResponse = await universalHttpService.AddAsync<AddUserLeaveLimitDto, UserLeaveLimitDtoODataResponse>("odata/UserLeaveLimits", entityToAdd, "User leave limit added successfully", jsonSerializerOptions);
+        var odataResponse =
+            await this.universalHttpService.AddAsync<AddUserLeaveLimitDto, UserLeaveLimitDtoODataResponse>(
+                "odata/UserLeaveLimits", entityToAdd, "User leave limit added successfully", JsonSerializerOptions);
         return odataResponse?.ToUserLeaveLimitDto();
-    } 
-    public Task<bool> EditAsync(UserLeaveLimitDto entityToEdit) => universalHttpService.EditAsync($"odata/UserLeaveLimits({entityToEdit.Id})", entityToEdit, "User leave limit edited successfully", jsonSerializerOptions);
+    }
+
+    public Task<bool> EditAsync(UserLeaveLimitDto entityToEdit) =>
+        this.universalHttpService.EditAsync(
+            $"odata/UserLeaveLimits({entityToEdit.Id})", entityToEdit, "User leave limit edited successfully",
+            JsonSerializerOptions);
 
     public Task<bool> DeleteAsync(Guid id) =>
-        universalHttpService.DeleteAsync($"odata/UserLeaveLimits({id})", "Successfully deleted leave limit", jsonSerializerOptions);
+        this.universalHttpService.DeleteAsync($"odata/UserLeaveLimits({id})", "Successfully deleted leave limit",
+            JsonSerializerOptions);
+
     public class UserLeaveLimitDtoODataResponse : UserLeaveLimitDto
     {
-        [JsonPropertyName(name: "@odata.context")]
-        public string? ContextUrl { get; set; }
+        [JsonPropertyName("@odata.context")] public string? ContextUrl { get; set; }
+
         public UserLeaveLimitDto ToUserLeaveLimitDto() =>
-            new(Id, Limit, OverdueLimit, LeaveTypeId, ValidSince, ValidUntil, Property);
+            new(this.Id, this.Limit, this.OverdueLimit, this.LeaveTypeId, this.ValidSince, this.ValidUntil,
+                this.Property);
     }
 }
-
