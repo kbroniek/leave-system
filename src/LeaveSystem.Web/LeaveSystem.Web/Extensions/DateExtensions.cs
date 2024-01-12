73a846a10599c9a0db8dc2763590e23411a45f8a
broni <broniekf@gmail,com>
2024-01-12 14:56:59 +0100

    fix culture

--- src/LeaveSystem.Web/LeaveSystem.Web/Extensions/DateExtensions.cs
+++ src/LeaveSystem.Web/LeaveSystem.Web/Extensions/DateExtensions.cs
@@ -1,10 +1,7 @@
 namespace LeaveSystem.Web.Extensions;
 
-using System.Globalization;
-
 public static class DateExtensions
 {
-    private static readonly CultureInfo Pl = new("pl-PL");
     public static string GetReadableDate(this DateTimeOffset? date) =>
-        date.HasValue ? date.Value.ToString("d", Pl) : "---";
+        date.HasValue ? date.Value.ToString("d", Thread.CurrentThread.CurrentCulture) : "---";
 }
