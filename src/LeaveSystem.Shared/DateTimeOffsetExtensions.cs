8d472805f6ce1f0fbdb50a78d4c69c6b6649028e
kbroniek <karol.broniek.gtm@gmail.com>
2024-02-29 14:13:51 +0100

    Create simple timeline component

--- src/LeaveSystem.Shared/DateTimeOffsetExtensions.cs
+++ src/LeaveSystem.Shared/DateTimeOffsetExtensions.cs
@@ -1,16 +1,16 @@
-﻿namespace LeaveSystem.Shared;
+namespace LeaveSystem.Shared;
 public static class DateTimeOffsetExtensions
 {
     public static DateTimeOffset GetDayWithoutTime(this DateTimeOffset date) =>
-        new DateTimeOffset(date.Ticks - (date.Ticks % TimeSpan.TicksPerDay), TimeSpan.Zero);
+        new(date.Ticks - (date.Ticks % TimeSpan.TicksPerDay), TimeSpan.Zero);
     public static DateTimeOffset GetFirstDayOfYear(this DateTimeOffset date) =>
         GetFirstDayOfYear(date.Year);
     public static DateTimeOffset GetLastDayOfYear(this DateTimeOffset date) =>
         GetLastDayOfYear(date.Year);
     public static DateTimeOffset GetFirstDayOfYear(int year) =>
-        new DateTimeOffset(year, 1, 1, 0, 0, 0, TimeSpan.Zero);
+        new(year, 1, 1, 0, 0, 0, TimeSpan.Zero);
     public static DateTimeOffset GetLastDayOfYear(int year) =>
-        new DateTimeOffset(year, 12, 31, 23, 59, 59, 999, TimeSpan.Zero);
+        new(year, 12, 31, 23, 59, 59, 999, TimeSpan.Zero);
     public static DateTimeOffset CreateFromDate(int year, int month, int day) =>
         new(year, month, day, 0, 0, 0, TimeSpan.Zero);
 }
