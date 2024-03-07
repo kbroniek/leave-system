f1407ae8c6ed17d4b492550abbd9d5ad53cb9129
kbroniek <karol.broniek.gtm@gmail.com>
2024-03-07 08:23:00 +0100

    Fix api link

--- src/LeaveSystem.Api/LeaveSystem.Api/Program.cs
+++ src/LeaveSystem.Api/LeaveSystem.Api/Program.cs
@@ -44,7 +44,7 @@ builder.Services.AddODataControllersServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
     app.UseSwagger();
