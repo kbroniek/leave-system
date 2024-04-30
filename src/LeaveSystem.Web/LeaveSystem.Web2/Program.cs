d7d71698581b522b53cf945a49a36f12bc96fb5f
kbroniek <karol.broniek.gtm@gmail.com>
2024-04-30 15:04:06 +0200

    Remove authentication

--- src/LeaveSystem.Web/LeaveSystem.Web2/Program.cs
+++ src/LeaveSystem.Web/LeaveSystem.Web2/Program.cs
@@ -8,9 +8,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddMsalAuthentication(options =>
-{
-    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-});
+//builder.Services.AddMsalAuthentication(options =>
+//{
+//    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+//});
 
 await builder.Build().RunAsync();
