236164ad09ab95a1afde51c342b332af3a227fda
kbroniek <karol.broniek.gtm@gmail.com>
2024-05-09 15:00:59 +0200

    Add auth for azure functions

--- src/LeaveSystem.Functions/LeaveSystem.Functions/Program.cs
+++ src/LeaveSystem.Functions/LeaveSystem.Functions/Program.cs
@@ -1,13 +1,40 @@
+using LeaveSystem.Shared.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Logging;
 
+IdentityModelEventSource.ShowPII = true;
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureFunctionsWebApplication(builder =>
+    {
+        // Explicitly adding the extension middleware because
+        // registering middleware when extension is loaded does not
+        // place the middleware in the pipeline where required request
+        // information is available.
+        builder.UseFunctionsAuthorization();
+    })
     .ConfigureServices(services =>
     {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
+        services
+            .AddFunctionsAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Authority = "https://leavesystem.b2clogin.com/tfp/2fefdb33-6a3f-432e-a52c-634f702535be/b2c_1_signin/v2.0/";
+                options.Audience = "4f24b978-403f-47fe-9cae-52deea03661d";
+
+            });
+
+        services.AddFunctionsAuthorization(options =>
+        {
+            options.AddPolicy("OnlyAdmins", policy =>
+                policy.Requirements.Add(new RoleRequirement(RoleType.GlobalAdmin)));
+        });
+        services.AddScoped<IAuthorizationHandler, RoleRequirementHandler>();
+
+        // Add other services
     })
     .Build();
 
