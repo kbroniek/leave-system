03b19a3254d797ebf748699f49ef10fe6658f104
kbroniek <karol.broniek.gtm@gmail.com>
2024-05-13 15:30:11 +0200

    Use roles

--- src/LeaveSystem.Functions/LeaveSystem.Functions/Program.cs
+++ src/LeaveSystem.Functions/LeaveSystem.Functions/Program.cs
@@ -5,15 +5,12 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 IdentityModelEventSource.ShowPII = true;
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication(builder =>
     {
-        // Explicitly adding the extension middleware because
-        // registering middleware when extension is loaded does not
-        // place the middleware in the pipeline where required request
-        // information is available.
         builder.UseFunctionsAuthorization();
     })
     .ConfigureServices(services =>
@@ -22,8 +19,13 @@ var host = new HostBuilder()
             .AddFunctionsAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options =>
             {
-                options.Authority = "https://leavesystem.b2clogin.com/tfp/2fefdb33-6a3f-432e-a52c-634f702535be/b2c_1_signin/v2.0/";
+                options.Authority = "https://leavesystem.b2clogin.com/tfp/leavesystem.onmicrosoft.com/b2c_1_signin/v2.0/";
                 options.Audience = "4f24b978-403f-47fe-9cae-52deea03661d";
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                    RoleClaimType = "extension_Role"
+                };
 
             });
 
