891b4a4909c17c32e42eb61fff43dbb94e043e73
kbroniek <karol.broniek.gtm@gmail.com>
2024-05-13 15:33:12 +0200

    Add function for get roles

---
+++ src/LeaveSystem.Functions/LeaveSystem.Functions/GetRoles.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace LeaveSystem.Functions
+{
+    public class GetRoles
+    {
+        private readonly ILogger<GetRoles> _logger;
+
+        public GetRoles(ILogger<GetRoles> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("GetRoles")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult(new string[] { "GlobalAdmin", "TestRole" });
+        }
+    }
+}
