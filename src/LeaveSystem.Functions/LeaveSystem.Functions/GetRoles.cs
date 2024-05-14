0417d79494267abc7d9445a1a3fbf2d3292127e2
kbroniek <karol.broniek.gtm@gmail.com>
2024-05-14 10:17:32 +0200

    Add basic auth

--- src/LeaveSystem.Functions/LeaveSystem.Functions/GetRoles.cs
+++ src/LeaveSystem.Functions/LeaveSystem.Functions/GetRoles.cs
@@ -1,8 +1,12 @@
+using System.Reflection.PortableExecutable;
+using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
 
 namespace LeaveSystem.Functions
 {
@@ -23,8 +27,47 @@ namespace LeaveSystem.Functions
         public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+            if (!ValidateToken(req.Headers.Authorization))
+            {
+                return new ObjectResult(new
+                {
+                    version = "1.0",
+                    status = 401,
+                    code = "errorCode",
+                    requestId = "requestId",
+                    userMessage = "Niepoprawne dane autoryzacyjne. Skonsultuj się z administratorem",
+                    developerMessage = $"The provided code auth {req.Headers.Authorization} does not match the expected login and password."
+                })
+                { StatusCode = 401 };
+            }
             Person person = await JsonSerializer.DeserializeAsync<Person>(req.Body, options);
             return new OkObjectResult(new { roles = new string[] { "GlobalAdmin", "TestRole", person.Email, person.Id } });
+        }
+        private bool ValidateToken(string header)
+        {
+            //Checking the header
+            if (!string.IsNullOrEmpty(header) && header.StartsWith("Basic"))
+            {
+                //Extracting credentials
+                // Removing "Basic " Substring
+                string encodedUsernamePassword = header.Substring("Basic ".Length).Trim();
+                //Decoding Base64
+                Encoding encoding = Encoding.GetEncoding("iso-8859-1");
+                string usernamePassword = encoding.GetString(Convert.FromBase64String(encodedUsernamePassword));
+                //Splitting Username:Password
+                int seperatorIndex = usernamePassword.IndexOf(':');
+                // Extracting the individual username and password
+                var username = usernamePassword.Substring(0, seperatorIndex);
+                var password = usernamePassword.Substring(seperatorIndex + 1);
+                //Validating the credentials
+                return username == Environment.GetEnvironmentVariable("RestApiUsername") &&
+                    password == Environment.GetEnvironmentVariable("RestApiPassword");
+            }
+            else
+            {
+                _logger.LogInformation($"Header is missing");
+                return false;
+            }
         }
     }
 
