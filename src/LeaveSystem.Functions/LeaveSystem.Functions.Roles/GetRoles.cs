938b9b32a6f0f709ec9a12be639c3cad47165efb
Karol Broniek <58724721+kbroniek@users.noreply.github.com>
2024-06-04 16:54:24 +0200

    Azure functions get roles (#92)
    * Update PROD api
    
    * Align changes (#88)
    
    * Optimize github actions (#86)
    
    * Build only changed projects
    
    * Use packages.locl.json
    
    * Publish production wasm
    
    * Optimize dotnet restore.
    
    * Add paths
    
    * Remove json loced
    
    * Update packages
    
    * Remove redundant file
    
    * Create badge (#87)
    
    * Create badge
    
    * Update badge
    
    * Update actions name
    
    * Feature/create status badge (#89)
    
    * Create badge
    
    * Update badge
    
    * Update actions name
    
    * Update badger
    
    * Add azure function for getting roles
    
    * Remove unused github workflows
    
    * Add github actions
    
    * Add project to solution
    
    * Add missing package
    
    * Test deploy
    
    * Add Microsoft.Azure.Functions.Worker.Extensions.CosmosDB
    
    * Read data from cosmosDB
    
    * Use develop branch for github actions
    
    * Reorder packages

---
+++ src/LeaveSystem.Functions/LeaveSystem.Functions.Roles/GetRoles.cs
@@ -0,0 +1,91 @@
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace LeaveSystem.Functions
+{
+    public class GetRoles
+    {
+        private static readonly JsonSerializerOptions jsonSerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+        private static readonly CosmosLinqSerializerOptions linqSerializerOptions = new CosmosLinqSerializerOptions
+        {
+            PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+        };
+        private readonly ILogger<GetRoles> _logger;
+
+        public GetRoles(ILogger<GetRoles> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("GetRoles")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req,
+            [CosmosDBInput(
+                databaseName: "LeaveSystem",
+                containerName: "Roles",
+                Connection = "CosmosDBConnection")] Container container)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            if (!ValidateToken(req.Headers.Authorization))
+            {
+                _logger.LogInformation("Wrong RestApi credentials.");
+                return new ObjectResult(new
+                {
+                    version = "1.0",
+                    status = 401,
+                    code = "errorCode",
+                    requestId = "requestId",
+                    userMessage = "Wrong RestApi credentials. Contact with administrators.",
+                    developerMessage = $"The provided code auth {req.Headers.Authorization} does not match the expected login and password."
+                })
+                { StatusCode = 401 };
+            }
+            var userId = await JsonSerializer.DeserializeAsync<UserId>(req.Body, jsonSerializerOptions);
+            _logger.LogInformation($"Deserialized user id {userId.Id}.");
+            var iterator = container.GetItemLinqQueryable<RolesModel>(linqSerializerOptions: linqSerializerOptions)
+                .Where(r => r.Id == userId.Id)
+                .ToFeedIterator();
+            var roles = iterator.HasMoreResults ?
+                (await iterator.ReadNextAsync()).FirstOrDefault()?.Roles :
+                null;
+            return new OkObjectResult(new { roles = roles ?? Enumerable.Empty<string>() });
+        }
+        private bool ValidateToken(string header)
+        {
+            //Checking the header
+            if (!string.IsNullOrEmpty(header) && header.StartsWith("Basic"))
+            {
+                //Extracting credentials
+                // Removing "Basic " Substring
+                string encodedUsernamePassword = header.Substring("Basic ".Length).Trim();
+                //Decoding Base64
+                Encoding encoding = Encoding.GetEncoding("iso-8859-1");
+                string usernamePassword = encoding.GetString(Convert.FromBase64String(encodedUsernamePassword));
+                //Splitting Username:Password
+                int seperatorIndex = usernamePassword.IndexOf(':');
+                // Extracting the individual username and password
+                var username = usernamePassword.Substring(0, seperatorIndex);
+                var password = usernamePassword.Substring(seperatorIndex + 1);
+                //Validating the credentials
+                return username == Environment.GetEnvironmentVariable("RestApiUsername") &&
+                    password == Environment.GetEnvironmentVariable("RestApiPassword");
+            }
+            else
+            {
+                _logger.LogWarning("Header is missing");
+                return false;
+            }
+        }
+    }
+
+    public record UserId(Guid Id);
+    public record RolesModel(Guid Id, string[] Roles);
+}
