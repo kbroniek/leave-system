bcd141507a07962028da92e0decc109cb8e800b6
kbroniek <karol.broniek.gtm@gmail.com>
2024-06-05 16:02:24 +0200

    Use ExceptionHandlingMiddleware

---
+++ src/LeaveSystem.Functions.Shared/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,63 @@
+namespace LeaveSystem.Functions.Shared;
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Middleware;
+using Microsoft.Extensions.Logging;
+
+// TODO: Remove when it is done https://github.com/Azure/azure-functions-dotnet-worker/issues/2476
+public class ExceptionHandlingMiddleware : IFunctionsWorkerMiddleware
+{
+    private static readonly JsonSerializerOptions SerializeOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = true
+    };
+    private readonly ILogger<ExceptionHandlingMiddleware> logger;
+
+    public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        this.logger = logger;
+    }
+    public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Global exception handler");
+            if (!await HandleHttpError(context, HttpStatusCode.InternalServerError, ex))
+            {
+                throw;
+            }
+        }
+    }
+
+    private static async Task<bool> HandleHttpError(FunctionContext context, HttpStatusCode status, Exception ex)
+    {
+        var httpContext = context.GetHttpContext();
+        if (httpContext is null)
+        {
+            return false;
+        }
+
+        httpContext.Response.StatusCode = (int)status;
+        var problemDetails = new ProblemDetails
+        {
+            Detail = ex.Message,
+            Status = (int)status,
+            Title = "Global exception handler"
+        };
+        httpContext.Response.Headers.ContentType = "application/problem+json; charset=utf-8";
+
+        await HttpResponseWritingExtensions.WriteAsync(httpContext.Response, JsonSerializer.Serialize(problemDetails, SerializeOptions));
+
+        return true;
+    }
+}
