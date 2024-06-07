1e5cc93a4146d6d11097fa650500aca70f36d721
kbroniek <karol.broniek.gtm@gmail.com>
2024-06-07 16:21:18 +0200

    Adjust search leave request

--- src/LeaveSystem.Shared/Dto/SearchLeaveRequestDto.cs
+++ src/LeaveSystem.Shared/Dto/SearchLeaveRequestDto.cs
@@ -3,13 +3,10 @@ using System;
 using LeaveSystem.Shared.LeaveRequests;
 
 public record SearchLeaveRequestDto(Guid Id,
-                                    DateTimeOffset DateFrom,
-                                    DateTimeOffset DateTo,
-                                    TimeSpan Duration,
-                                    LeaveRequestStatus Status,
-                                    Guid CreatedBy,
-                                    TimeSpan WorkingHours,
-                                    SearchLeaveRequestDto.LeaveTypeDto LeaveType)
-{
-    public record LeaveTypeDto(Guid LeaveTypeId, string Name, string Color);
-}
+    DateTimeOffset DateFrom,
+    DateTimeOffset DateTo,
+    TimeSpan Duration,
+    Guid LeaveTypeId,
+    LeaveRequestStatus Status,
+    string CreatedById,
+    TimeSpan WorkingHours);
