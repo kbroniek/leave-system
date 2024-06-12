e4719c3b2f5e1488be15bc77f863bc22a2d50d7f
kbroniek <karol.broniek.gtm@gmail.com>
2024-06-12 15:24:50 +0200

    Fix build

--- src/LeaveSystem.Shared/Dto/PagedListResponse.cs
+++ src/LeaveSystem.Shared/Dto/PagedListResponse.cs
@@ -2,7 +2,7 @@ namespace LeaveSystem.Shared.Dto;
 
 public static class PagedListResponseExtensions
 {
-    public static PagedListResponse<T> ToPagedListResponse<T>(this IEnumerable<T>? items) => new(items, items.LongCount(), false);
+    public static PagedListResponse<T> ToPagedListResponse<T>(this IEnumerable<T>? items) => new(items, items?.LongCount() ?? 0, false);
     public static PagedListResponse<T> ToPagedListResponse<T>(this IEnumerable<T>? items, long totalItemCount, bool hasNextPage) => new(items, totalItemCount, hasNextPage);
 }
 
--- src/LeaveSystem.Shared/Result.cs
+++ src/LeaveSystem.Shared/Result.cs
@@ -1,59 +1,32 @@
 namespace LeaveSystem.Shared;
 
 using System;
+public static class Result
+{
+    public static Result<TValue, TError> Ok<TValue, TError>(TValue v) => new(v, default, true);
+    public static Result<TValue, TError> Err<TValue, TError>(TError e) => new(default, e, false);
+}
 
 public readonly struct Result<TValue, TError>
 {
-    private readonly TValue? _value;
-    private readonly TError? _error;
+    private readonly bool success;
+    public readonly TValue Value;
+    public readonly TError Error;
 
-    public Result(TError? error)
+    internal Result(TValue v, TError e, bool success)
     {
-        Value = value;
-        Error = error;
-        _success = success;
+        Value = v;
+        Error = e;
+        this.success = success;
     }
 
-    public bool IsOk => _success;
+    public bool IsOk => success;
 
-    public static Result<T, E> Ok(T v)
-    {
-        return new(v, default(E), true);
-    }
-
-    public static Result<T, E> Err(E e)
-    {
-        return new(default(T), e, false);
-    }
-
-    public static implicit operator Result<T, E>(T v) => new(v, default(E), true);
-    public static implicit operator Result<T, E>(E e) => new(default(T), e, false);
+    public static implicit operator Result<TValue, TError>(TValue v) => new(v, default, true);
+    public static implicit operator Result<TValue, TError>(TError e) => new(default, e, false);
 
     public R Match<R>(
-            Func<T, R> success,
-            Func<E, R> failure) =>
-        _success ? success(Value) : failure(Error);
-}
-
-public readonly struct Result<TError>
-{
-    public bool IsOk { get; }
-
-    private readonly TError? _error;
-
-    public Result(TError? error)
-    {
-        _error = error;
-        IsOk = false;
-    }
-
-    public static implicit operator Result<TError>(TError e) => new(e);
-
-    public TResult Match<TResult>(
-            Func<TResult> success,
-            Func<TError, TResult> failure) =>
-    IsOk ? success() : failure(_error!);
-
-    public Task<TResult> MatchAsync<TResult>(Func<Task<TResult>> success, Func<TError, Task<TResult>> failure) =>
-        IsOk ? success() : failure(_error!);
+            Func<TValue, R> success,
+            Func<TError, R> failure) =>
+        this.success ? success(Value) : failure(Error);
 }
