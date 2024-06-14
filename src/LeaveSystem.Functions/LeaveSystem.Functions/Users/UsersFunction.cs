3630b9290138dc509b787bdfe851822975a01c26
kbroniek <karol.broniek.gtm@gmail.com>
2024-06-14 11:25:00 +0200

    CRUD users

--- src/LeaveSystem.Functions/LeaveSystem.Functions/Users/UsersFunction.cs
+++ src/LeaveSystem.Functions/LeaveSystem.Functions/Users/UsersFunction.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using FromBodyAttribute = Microsoft.Azure.Functions.Worker.Http.FromBodyAttribute;
 
 public class UsersFunction
 {
@@ -20,7 +21,7 @@ public class UsersFunction
 
     [Function(nameof(GetUsers))]
     [Authorize(Roles = $"{nameof(RoleType.GlobalAdmin)},{nameof(RoleType.UserAdmin)}")]
-    public IActionResult GetUsers([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
+    public IActionResult GetUsers([HttpTrigger(AuthorizationLevel.Function, "get", Route = "users")] HttpRequest req)
     {
         logger.LogInformation("C# HTTP trigger function processed a request.");
         var userId = req.HttpContext.GetUserId();
@@ -29,5 +30,30 @@ public class UsersFunction
             new UserDto(userId, req.HttpContext.User.Identity?.Name, "test@email.com", new [] { nameof(RoleType.GlobalAdmin) })
         };
         return new OkObjectResult(users.ToPagedListResponse());
+    }
+
+    [Function(nameof(EditUser))]
+    [Authorize(Roles = $"{nameof(RoleType.GlobalAdmin)},{nameof(RoleType.UserAdmin)}")]
+    public IActionResult EditUser([HttpTrigger(AuthorizationLevel.Function, "put", Route = "users/{userId:alpha}")] HttpRequest req, string userId, [FromBody] EditUserDto user)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult(new UserDto(userId, user.Name, user.Email, user.Roles));
+    }
+
+    [Function(nameof(CreateUser))]
+    [Authorize(Roles = $"{nameof(RoleType.GlobalAdmin)},{nameof(RoleType.UserAdmin)}")]
+    public IActionResult CreateUser([HttpTrigger(AuthorizationLevel.Function, "post", Route = "users")] HttpRequest req, [FromBody] CreateUserDto user)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+        var userId = Guid.NewGuid().ToString();
+        return new CreatedResult($"/users/{userId}", new UserDto(userId, user.Name, user.Email, user.Roles));
+    }
+
+    [Function(nameof(DisableUser))]
+    [Authorize(Roles = $"{nameof(RoleType.GlobalAdmin)},{nameof(RoleType.UserAdmin)}")]
+    public IActionResult DisableUser([HttpTrigger(AuthorizationLevel.Function, "delete", Route = "users/{userId:alpha}")] HttpRequest req, string userId)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new NoContentResult();
     }
 }
--- src/LeaveSystem.Shared/Dto/UserDto.cs
+++ src/LeaveSystem.Shared/Dto/UserDto.cs
@@ -2,3 +2,6 @@ namespace LeaveSystem.Shared.Dto;
 using System.Collections.Generic;
 
 public record UserDto(string Id, string? Name, string? Email, IEnumerable<string>? Roles);
+
+public record EditUserDto(string Name, string Email, IEnumerable<string>? Roles);
+public record CreateUserDto(string Name, string Email, IEnumerable<string>? Roles);
