d11f4899dce80599ce169ec25286c68df00b9369
kbroniek <karol.broniek.gtm@gmail.com>
2024-06-18 17:06:45 +0200

    Add Leave Type CRUD

--- src/LeaveSystem.Functions/LeaveSystem.Functions/LeaveTypes/LeaveTypesFunction.cs
+++ src/LeaveSystem.Functions/LeaveSystem.Functions/LeaveTypes/LeaveTypesFunction.cs
@@ -1,3 +1,4 @@
+using LeaveSystem.Functions.Extensions;
 using LeaveSystem.Shared;
 using LeaveSystem.Shared.Auth;
 using LeaveSystem.Shared.Dto;
@@ -7,91 +8,123 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace LeaveSystem.Functions.LeaveTypes
+namespace LeaveSystem.Functions.LeaveTypes;
+
+public class LeaveTypesFunction
 {
-    public class LeaveTypesFunction
+    private static GetLeaveTypeDto holidayLeave = new(
+        Guid.Parse("ae752d4b-0368-4d46-8efa-9ef2ee248fa9"),
+        "urlop wypoczynkowy",
+        1,
+        new GetLeaveTypeDto.LeaveTypeDtoProperties(
+            DefaultLimitDays: 26,
+            IncludeFreeDays: false,
+            Color: "#0137C9",
+            Catalog: LeaveTypeCatalog.Holiday
+        )
+    );
+
+    private static GetLeaveTypeDto onDemandLeave = new(
+        Id: Guid.Parse("6e1a75ca-07dc-45aa-9451-57e1cefd7ee6"),
+        Name: "urlop na żądanie",
+        BaseLeaveTypeId: holidayLeave.Id,
+        Order: 2,
+        Properties: new GetLeaveTypeDto.LeaveTypeDtoProperties(
+            DefaultLimitDays: 4,
+            IncludeFreeDays: false,
+            Color: "#B88E1E",
+            Catalog: LeaveTypeCatalog.OnDemand
+        )
+    );
+
+    private static GetLeaveTypeDto sickLeave = new(
+        Id: Guid.Parse("c91558cc-5d58-4a77-b46e-e6a69ac14cf4"),
+        Name: "niezdolność do pracy z powodu choroby",
+        Order: 3,
+        Properties: new GetLeaveTypeDto.LeaveTypeDtoProperties(
+            IncludeFreeDays: true,
+            Color: "#FF3333",
+            Catalog: LeaveTypeCatalog.Sick
+        )
+    );
+
+    private static GetLeaveTypeDto saturdayLeave = new(
+        Id: Guid.Parse("2dfb7d85-70d8-4acc-a565-62fde19b7cd1"),
+        Name: "urlop za sobotę",
+        Order: 14,
+        Properties: new GetLeaveTypeDto.LeaveTypeDtoProperties(
+            DefaultLimitDays: 1,
+            IncludeFreeDays: false,
+            Color: "#FFFF33",
+            Catalog: LeaveTypeCatalog.Saturday
+        )
+    );
+    private readonly ILogger logger;
+
+    public LeaveTypesFunction(ILoggerFactory loggerFactory) => logger = loggerFactory.CreateLogger<LeaveTypesFunction>();
+
+    [Function(nameof(GetLeaveTypes))]
+    [Authorize(Roles = $"{nameof(RoleType.GlobalAdmin)},{nameof(RoleType.Employee)}")]
+    public async Task<IActionResult> GetLeaveTypes([HttpTrigger(
+        AuthorizationLevel.Anonymous,
+        "get",
+        Route = "leavetypes")] HttpRequest req)
     {
-        private static GetLeaveTypeDto holidayLeave = new(
-            Guid.Parse("ae752d4b-0368-4d46-8efa-9ef2ee248fa9"),
-            "urlop wypoczynkowy",
-            1,
-            new GetLeaveTypeDto.LeaveTypeDtoProperties(
-                DefaultLimitDays: 26,
-                IncludeFreeDays: false,
-                Color: "#0137C9",
-                Catalog: LeaveTypeCatalog.Holiday
-            )
-        );
+        logger.LogInformation("C# HTTP trigger function processed a request.");
 
-        private static GetLeaveTypeDto onDemandLeave = new(
-            Id: Guid.Parse("6e1a75ca-07dc-45aa-9451-57e1cefd7ee6"),
-            Name: "urlop na żądanie",
-            BaseLeaveTypeId: holidayLeave.Id,
-            Order: 2,
-            Properties: new GetLeaveTypeDto.LeaveTypeDtoProperties(
-                DefaultLimitDays: 4,
-                IncludeFreeDays: false,
-                Color: "#B88E1E",
-                Catalog: LeaveTypeCatalog.OnDemand
-            )
-        );
-
-        private static GetLeaveTypeDto sickLeave = new(
-            Id: Guid.Parse("c91558cc-5d58-4a77-b46e-e6a69ac14cf4"),
-            Name: "niezdolność do pracy z powodu choroby",
-            Order: 3,
-            Properties: new GetLeaveTypeDto.LeaveTypeDtoProperties(
-                IncludeFreeDays: true,
-                Color: "#FF3333",
-                Catalog: LeaveTypeCatalog.Sick
-            )
-        );
-
-        private static GetLeaveTypeDto saturdayLeave = new(
-            Id: Guid.Parse("2dfb7d85-70d8-4acc-a565-62fde19b7cd1"),
-            Name: "urlop za sobotę",
-            Order: 14,
-            Properties: new GetLeaveTypeDto.LeaveTypeDtoProperties(
-                DefaultLimitDays: 1,
-                IncludeFreeDays: false,
-                Color: "#FFFF33",
-                Catalog: LeaveTypeCatalog.Saturday
-            )
-        );
-        private readonly ILogger logger;
-
-        public LeaveTypesFunction(ILoggerFactory loggerFactory) => logger = loggerFactory.CreateLogger<LeaveTypesFunction>();
-
-        [Function(nameof(GetLeaveTypes))]
-        [Authorize(Roles = $"{nameof(RoleType.GlobalAdmin)},{nameof(RoleType.Employee)}")]
-        public async Task<IActionResult> GetLeaveTypes([HttpTrigger(
-            AuthorizationLevel.Anonymous,
-            "get",
-            Route = "leavetypes")] HttpRequest req)
+        var leaveTypes = new GetLeaveTypeDto[]
         {
-            logger.LogInformation("C# HTTP trigger function processed a request.");
+            holidayLeave,
+            onDemandLeave,
+            sickLeave,
+            saturdayLeave
+        };
 
-            var leaveTypes = new GetLeaveTypeDto[]
-            {
-                holidayLeave,
-                onDemandLeave,
-                sickLeave,
-                saturdayLeave
-            };
+        return new OkObjectResult(leaveTypes);
+    }
 
-            return new OkObjectResult(leaveTypes);
-        }
+    [Function(nameof(GetLeaveType))]
+    [Authorize(Roles = $"{nameof(RoleType.GlobalAdmin)},{nameof(RoleType.Employee)},{nameof(RoleType.DecisionMaker)}")]
+    public async Task<IActionResult> GetLeaveType([HttpTrigger(
+        AuthorizationLevel.Anonymous,
+        "get",
+        Route = "leavetypes/{leaveTypeId:guid}")] HttpRequest req, Guid leaveTypeId)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request.");
 
-        [Function(nameof(GetLeaveType))]
-        [Authorize(Roles = $"{nameof(RoleType.GlobalAdmin)},{nameof(RoleType.Employee)},{nameof(RoleType.DecisionMaker)}")]
-        public async Task<IActionResult> GetLeaveType([HttpTrigger(
-            AuthorizationLevel.Anonymous,
-            "get",
-            Route = "leavetypes/{leaveTypeId:guid}")] HttpRequest req, Guid leaveTypeId)
-        {
-            logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult(holidayLeave);
+    }
 
-            return new OkObjectResult(holidayLeave);
-        }
+    [Function(nameof(CreateLeaveType))]
+    [Authorize(Roles = $"{nameof(RoleType.GlobalAdmin)},{nameof(RoleType.LeaveTypeAdmin)}")]
+    public IActionResult CreateLeaveType([HttpTrigger(
+        AuthorizationLevel.Anonymous,
+        "post",
+        Route = "leavetypes")] HttpRequest req)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new CreatedResult($"/leavetypes/{holidayLeave.Id}", holidayLeave);
+    }
+
+    [Function(nameof(UpdateLeaveType))]
+    [Authorize(Roles = $"{nameof(RoleType.GlobalAdmin)},{nameof(RoleType.LeaveTypeAdmin)}")]
+    public IActionResult UpdateLeaveType([HttpTrigger(
+        AuthorizationLevel.Anonymous,
+        "put",
+        Route = "leavetypes")] HttpRequest req)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult(holidayLeave);
+    }
+
+    [Function(nameof(RemoveLeaveType))]
+    [Authorize(Roles = $"{nameof(RoleType.GlobalAdmin)},{nameof(RoleType.LeaveTypeAdmin)}")]
+    public IActionResult RemoveLeaveType([HttpTrigger(
+        AuthorizationLevel.Anonymous,
+        "delete",
+        Route = "leavetypes/{leaveTypeId:guid}")] HttpRequest req, Guid leaveTypeId)
+    {
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new NoContentResult();
     }
 }
