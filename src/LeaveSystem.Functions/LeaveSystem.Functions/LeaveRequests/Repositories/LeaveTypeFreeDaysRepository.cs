884081c7de140cb67f52575fd1912ef8fd97b574
kbroniek <karol.broniek.gtm@gmail.com>
2024-08-30 08:42:16 +0200

    Implement LeaveTypeFreeDaysRepository

--- src/LeaveSystem.Functions/LeaveSystem.Functions/LeaveRequests/Repositories/LeaveTypeFreeDaysRepository.cs
+++ src/LeaveSystem.Functions/LeaveSystem.Functions/LeaveRequests/Repositories/LeaveTypeFreeDaysRepository.cs
@@ -4,8 +4,31 @@ using System.Threading.Tasks;
 using LeaveSystem.Domain;
 using LeaveSystem.Domain.LeaveRequests.Creating.Validators;
 using LeaveSystem.Shared;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
 
-public class LeaveTypeFreeDaysRepository : ILeaveTypeFreeDaysRepository
+public class LeaveTypeFreeDaysRepository(CosmosClient cosmosClient, string databaseName, string containerId) : ILeaveTypeFreeDaysRepository
 {
-    public Task<Result<bool?, Error>> IsIncludeFreeDays(Guid leaveTypeId, CancellationToken cancellationToken) => throw new NotImplementedException();
+    public async Task<Result<bool?, Error>> IsIncludeFreeDays(Guid leaveTypeId, CancellationToken cancellationToken)
+    {
+        var container = cosmosClient.GetContainer(databaseName, containerId);
+        var iterator = container.GetItemLinqQueryable<Entity>()
+            .Where(x => x.Id == leaveTypeId)
+            .ToFeedIterator();
+
+        if (iterator.HasMoreResults)
+        {
+            var response = await iterator.ReadNextAsync(cancellationToken);
+            var entity = response.FirstOrDefault();
+            if (entity is not null)
+            {
+                return entity.Properties.IncludeFreeDays;
+            }
+        }
+        return new Error($"Cannot find the leave type. Id={leaveTypeId}", System.Net.HttpStatusCode.NotFound);
+    }
+
+    private sealed record Entity(Guid Id, EntityProperties Properties);
+
+    private sealed record EntityProperties(bool? IncludeFreeDays);
 }
