a5585261dcc26ed37f0ccb431265da287ce94494
kbroniek <karol.broniek.gtm@gmail.com>
2024-08-30 12:08:07 +0200

    Add DateOnly calculator

---
+++ src/LeaveSystem.Shared/LeaveSystem.Shared/DateOnlyExtensions.cs
@@ -0,0 +1,12 @@
+namespace LeaveSystem.Shared;
+public static class DateOnlyExtensions
+{
+    public static DateOnly GetFirstDayOfYear(this DateOnly date) =>
+        GetFirstDayOfYear(date.Year);
+    public static DateOnly GetLastDayOfYear(this DateOnly date) =>
+        GetLastDayOfYear(date.Year);
+    public static DateOnly GetFirstDayOfYear(int year) =>
+        new(year, 1, 1);
+    public static DateOnly GetLastDayOfYear(int year) =>
+        new(year, 12, 31);
+}
