bc60c46b43c9f74327be2ed998ba965807541ddc
kbroniek <karol.broniek.gtm@gmail.com>
2024-09-19 14:44:59 +0200

    Fix token claims

--- src/web/src/ui-components/TokenClaims.tsx
+++ src/web/src/ui-components/TokenClaims.tsx
@@ -2,10 +2,11 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 
-export const TokenClaims = (tokenClaims: {[key: string]: unknown}) => {
+export const TokenClaims = (props: {tokenClaims: {[key: string]: unknown} | undefined}) => {
     return (
+        props.tokenClaims ?
         <List id="idTokenClaims">
-            {Object.entries(tokenClaims).map(claim=> {
+            {Object.entries(props.tokenClaims).map(claim=> {
                 return (
                     <ListItem key={`claim-${claim[0]}`}>
                         <ListItemText primary={claim[0]} secondary={claim[1] as string}/>
@@ -13,5 +14,6 @@ export const TokenClaims = (tokenClaims: {[key: string]: unknown}) => {
                 );
             })}
         </List>
+        : null
     );
 };
