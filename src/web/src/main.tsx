47bc8655ee13dba28a3167f0a43755c5cf05dcb6
kbroniek <karol.broniek.gtm@gmail.com>
2024-09-20 14:08:21 +0200

    Remove top-level-await

--- src/web/src/main.tsx
+++ src/web/src/main.tsx
@@ -13,32 +13,33 @@ import { App } from "./App";
 import "./index.css";
 
 export const msalInstance = new PublicClientApplication(msalConfig);
-await msalInstance.initialize();
 
-// Optional - This will update account state if a user signs in from another tab or window
-msalInstance.enableAccountStorageEvents();
+msalInstance.initialize().then(() => {
+  // Optional - This will update account state if a user signs in from another tab or window
+  msalInstance.enableAccountStorageEvents();
 
-// Default to using the first account if no account is active on page load
-const accounts = msalInstance.getAllAccounts();
-if (!msalInstance.getActiveAccount() && accounts.length > 0) {
-  // Account selection logic is app dependent. Adjust as needed for different use cases.
-  msalInstance.setActiveAccount(accounts[0]);
-}
-
-msalInstance.addEventCallback((event: EventMessage) => {
-  if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
-    const payload = event.payload as AuthenticationResult;
-    const account = payload.account;
-    msalInstance.setActiveAccount(account);
+  // Default to using the first account if no account is active on page load
+  const accounts = msalInstance.getAllAccounts();
+  if (!msalInstance.getActiveAccount() && accounts.length > 0) {
+    // Account selection logic is app dependent. Adjust as needed for different use cases.
+    msalInstance.setActiveAccount(accounts[0]);
   }
-});
 
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App pca={msalInstance} />
-      </ThemeProvider>
-    </BrowserRouter>
-  </StrictMode>
-);
+  msalInstance.addEventCallback((event: EventMessage) => {
+    if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
+      const payload = event.payload as AuthenticationResult;
+      const account = payload.account;
+      msalInstance.setActiveAccount(account);
+    }
+  });
+
+  createRoot(document.getElementById("root") as HTMLElement).render(
+    <StrictMode>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App pca={msalInstance} />
+        </ThemeProvider>
+      </BrowserRouter>
+    </StrictMode>
+  );
+})
