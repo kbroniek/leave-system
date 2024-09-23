c84387a12d5fa27c32695dab4b0b1c5bbd2b8808
kbroniek <karol.broniek.gtm@gmail.com>
2024-09-23 12:33:47 +0200

    Add navbar

--- src/web/src/ui-components/NavBar.tsx
+++ src/web/src/ui-components/NavBar.tsx
@@ -1,25 +1,52 @@
+import { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 import WelcomeName from "./WelcomeName";
 import SignInSignOutButton from "./SignInSignOutButton";
 import { Link as RouterLink } from "react-router-dom";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItemText from "@mui/material/ListItemText";
+import { ListItemButton } from "@mui/material";
 
 const NavBar = () => {
-    return (
-        <div>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography sx={{ flexGrow: 1 }}>
-                        <Link component={RouterLink} to="/" color="inherit" variant="h6">Leave System</Link>
-                    </Typography>
-                    <WelcomeName />
-                    <SignInSignOutButton />
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  return (
+    <AppBar variant="outlined" position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={() => setIsDrawerOpen(true)}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography sx={{ flexGrow: 1 }}>
+            <Link component={RouterLink} to="/" color="inherit" variant="h6">Leave System</Link>
+        </Typography>
+        <WelcomeName />
+        <SignInSignOutButton />
+
+        <Drawer open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+          <List>
+            <ListItemButton component={RouterLink} to="/" onClick={() => setIsDrawerOpen(false)}>
+              <ListItemText primary="Home" />
+            </ListItemButton>
+
+            <ListItemButton component={RouterLink} to="/data" onClick={() => setIsDrawerOpen(false)}>
+              <ListItemText primary="Call API" />
+            </ListItemButton>
+          </List>
+        </Drawer>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default NavBar;
