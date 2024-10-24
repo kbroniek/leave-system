1d082c3a4ded16ab46b90754ed1df2421e6ba565
kbroniek <karol.broniek.gtm@gmail.com>
2024-10-24 14:50:43 +0200

    Replace elements to grid

--- src/web/src/ui-components/leave-request-details/ShowLeaveRequestDetails.tsx
+++ src/web/src/ui-components/leave-request-details/ShowLeaveRequestDetails.tsx
@@ -1,128 +1,93 @@
-import * as React from "react";
-
-import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import Grid2 from "@mui/material/Grid2";
 import { LeaveRequestDetailsDto } from "./LeaveRequestDetailsDto";
 import { LeaveTypeDto } from "../dtos/LeaveTypesDto";
 import Divider from "@mui/material/Divider";
 import { DaysCounter } from "../utils/DaysCounter";
 import { HolidaysDto } from "../leave-requests/HolidaysDto";
-import { DateTime } from "luxon";
+import { DateTime, DateTimeFormatOptions } from "luxon";
 import { DurationFormatter } from "../utils/DurationFormatter";
+import Grid from '@mui/material/Grid2';
+import Box from '@mui/material/Box';
 
-export default function ShowLeaveRequestsTimeline(params: {
+export default function ShowLeaveRequestsTimeline(params: Readonly<{
   leaveRequest: LeaveRequestDetailsDto;
   statusColor: string,
   leaveType: LeaveTypeDto
   holidays: HolidaysDto
-}): JSX.Element {
-    const defaultStyle = { paddingTop: "1px" };
-    const leaveTypeStyle = { ...defaultStyle, borderBottomColor: params.leaveType.properties?.color ?? "transparent" , borderBottomStyle: "solid" };
-    const leaveStatusStyle = { ...defaultStyle, borderBottomColor: params.statusColor, borderBottomStyle: "solid" };
-    const holidaysDateTime = params.holidays.items.map(x => DateTime.fromISO(x));
-    const daysCounter = new DaysCounter(params.leaveType.properties?.includeFreeDays ?? false, holidaysDateTime);
-    const dateFrom = DateTime.fromISO(params.leaveRequest.dateFrom);
-    const dateTo = DateTime.fromISO(params.leaveRequest.dateTo);
-    const createdDate = DateTime.fromISO(params.leaveRequest.createdDate);
+}>): JSX.Element {
+  const titleStyle = {color: "text.secondary", textAlign: "right"};
+  const defaultStyle = { paddingTop: "1px", width: "max-content" };
+  const leaveTypeStyle = { ...defaultStyle, borderBottomColor: params.leaveType.properties?.color ?? "transparent" , borderBottomStyle: "solid" };
+  const leaveStatusStyle = { ...defaultStyle, borderBottomColor: params.statusColor, borderBottomStyle: "solid" };
+  const holidaysDateTime = params.holidays.items.map(x => DateTime.fromISO(x));
+  const daysCounter = new DaysCounter(params.leaveType.properties?.includeFreeDays ?? false, holidaysDateTime);
+  const dateFrom = DateTime.fromISO(params.leaveRequest.dateFrom);
+  const dateTo = DateTime.fromISO(params.leaveRequest.dateTo);
+  const createdDate = DateTime.fromISO(params.leaveRequest.createdDate);
+  const lastModifiedDate = DateTime.fromISO(params.leaveRequest.lastModifiedDate);
+  const formatDate: DateTimeFormatOptions = { weekday: 'short', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' };
   return (
-    <Stack spacing={2} margin={2}>
-      <Typography variant="h5">{params.leaveRequest.assignedTo.name}</Typography>
-      <Divider />
-      <div>
-        <Grid2 container>
-            <React.Fragment key={params.leaveRequest.leaveTypeId}>
-              <Stack
-                direction="row"
-                spacing={1}
-                useFlexGap
-                sx={{ width: "100%", mb: 1 }}
-              >
-              <Typography variant="body1" sx={{ color: "text.secondary" }}>
-                Request type:
-              </Typography>
-              <Typography variant="body2" sx={leaveTypeStyle}>{params.leaveType.name}</Typography>
-            </Stack>
-              <Stack
-                direction="row"
-                spacing={1}
-                useFlexGap
-                sx={{ width: "100%", mb: 1 }}
-              >
-                <Typography variant="body1" sx={{ color: "text.secondary" }}>
-                    Leave from - to:
-                </Typography>
-                <Typography variant="body2" sx={defaultStyle}>{params.leaveRequest.dateFrom} - {params.leaveRequest.dateTo}</Typography>
-              </Stack>
-              <Stack
-                direction="row"
-                spacing={1}
-                useFlexGap
-                sx={{ width: "100%", mb: 1 }}
-              >
-                <Typography variant="body1" sx={{ color: "text.secondary" }}>
-                    Days:
-                </Typography>
-                <Typography variant="body2" sx={defaultStyle}>{daysCounter.days(dateFrom, dateTo)}</Typography>
-              </Stack>
-              <Stack
-                direction="row"
-                spacing={1}
-                useFlexGap
-                sx={{ width: "100%", mb: 1 }}
-              >
-                <Typography variant="body1" sx={{ color: "text.secondary" }}>
-                    Hours:
-                </Typography>
-                <Typography variant="body2" sx={defaultStyle}>{DurationFormatter.format(params.leaveRequest.duration)}</Typography>
-              </Stack>
-              <Stack
-                direction="row"
-                spacing={1}
-                useFlexGap
-                sx={{ width: "100%", mb: 1 }}
-              >
-                <Typography variant="body1" sx={{ color: "text.secondary" }}>
-                    Created date:
-                </Typography>
-                <Typography variant="body2" sx={defaultStyle}>{createdDate.toLocaleString({ weekday: 'short', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' })}</Typography>
-              </Stack>
-              <Stack
-                direction="row"
-                spacing={1}
-                useFlexGap
-                sx={{ width: "100%", mb: 1 }}
-              >
-                <Typography variant="body1" sx={{ color: "text.secondary" }}>
-                    Status:
-                </Typography>
-                <Typography variant="body2" sx={leaveStatusStyle}>{params.leaveRequest.status}</Typography>
-              </Stack>
-              <Stack
-                direction="row"
-                spacing={1}
-                useFlexGap
-                sx={{ width: "100%", mb: 1 }}
-              >
-                <Typography variant="body1" sx={{ color: "text.secondary" }}>
-                    Last modified:
-                </Typography>
-                <Typography variant="body2" sx={defaultStyle}>{params.leaveRequest.lastModifiedBy.name}</Typography>
-              </Stack>
-              <Stack
-                direction="row"
-                spacing={1}
-                useFlexGap
-                sx={{ width: "100%", mb: 1 }}
-              >
-                <Typography variant="body1" sx={{ color: "text.secondary" }}>
-                    Remarks:
-                </Typography>
-                <Typography variant="body2" sx={defaultStyle}>{params.leaveRequest.remarks.map(x => x.remarks).join(" | ")}</Typography>
-              </Stack>
-            </React.Fragment>
-        </Grid2>
-      </div>
-    </Stack>
+    <Box sx={{ flexGrow: 1 }} margin={2}>
+      <Grid container spacing={2}>
+        <Grid size={12}>
+          <Typography variant="h5">{params.leaveRequest.assignedTo.name}</Typography>
+          <Divider />
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body1" sx={titleStyle}>Request type:</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body2" sx={leaveTypeStyle}>{params.leaveType.name}</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body1" sx={titleStyle}>Leave from - to:</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body2" sx={defaultStyle}>{params.leaveRequest.dateFrom} - {params.leaveRequest.dateTo}</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body1" sx={titleStyle}>Days:</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body2" sx={defaultStyle}>{daysCounter.days(dateFrom, dateTo)}</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body1" sx={titleStyle}>Hours:</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body2" sx={defaultStyle}>{DurationFormatter.format(params.leaveRequest.duration)}</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body1" sx={titleStyle}>Created date:</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body2" sx={defaultStyle}>{createdDate.toLocaleString(formatDate)}</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body1" sx={titleStyle}>Status:</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body2" sx={leaveStatusStyle}>{params.leaveRequest.status}</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body1" sx={titleStyle}>Last modified by:</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body2" sx={defaultStyle}>{params.leaveRequest.lastModifiedBy.name}</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body1" sx={titleStyle}>Last modified date:</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body2" sx={defaultStyle}>{lastModifiedDate.toLocaleString(formatDate)}</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body1" sx={titleStyle}>Remarks:</Typography>
+        </Grid>
+        <Grid size={6}>
+          <Typography variant="body2" sx={defaultStyle}>{params.leaveRequest.remarks.map(x => x.remarks).join(" | ")}</Typography>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
