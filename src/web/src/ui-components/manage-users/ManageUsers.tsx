e6148372c74d9645de3779eafeb5b1877b3bb3da
kbroniek <karol.broniek.gtm@gmail.com>
2024-12-05 16:40:39 +0100

    Get data from backend

--- src/web/src/ui-components/manage-users/ManageUsers.tsx
+++ src/web/src/ui-components/manage-users/ManageUsers.tsx
@@ -9,77 +9,39 @@ import { useNotifications } from "@toolpad/core/useNotifications";
 import { callApiGet, ifErrorAcquireTokenRedirect } from "../../utils/ApiCall";
 import { Authorized } from "../../components/Authorized";
 import { Forbidden } from "../../components/Forbidden";
-import { UsersDto } from "../dtos/UsersDto";
+import { UserDto, UsersDto } from "../dtos/UsersDto";
 
 const DataContent = () => {
   const { instance, inProgress } = useMsal();
   const notifications = useNotifications();
   const [apiUsers, setApiUsers] = useState<UsersDto | undefined>();
 
+  const handleUserChange = async (user: UserDto): Promise<void> => {
+    //TODO: Save new value
+    console.log(user);
+  };
+
   useEffect(() => {
     if (inProgress === InteractionStatus.None) {
       callApiGet<UsersDto>("/users", notifications.show)
         .then((response) => setApiUsers(response))
         .catch((e) => ifErrorAcquireTokenRedirect(e, instance));
-      //TODO: Wait for API
-      //   callApiGet<RolesDto>("/roles", notifications.show)
-      //     .then((response) => setApiUsers(response))
-      //     .catch((e) => ifErrorAcquireTokenRedirect(e, instance));
-      setApiUsers({
-        items: [
-          {
-            id: "4e67827b-f93a-40c3-96f1-9193bdfac06d",
-            name: "",
-            firstName: "Olgierd",
-            lastName: "Urbanek",
-            department: "Lekarz",
-            roles: ["DecisionMaker", "Employee"],
-          },
-          {
-            id: "dca20276-2a30-43b0-89ce-3b0d0bd8501a",
-            name: "",
-            firstName: "Karol",
-            lastName: "Volt",
-            department: "Administrator",
-            roles: ["GlobalAdmin"],
-          },
-          {
-            id: "1769e6ea-a351-45bb-a518-a08f59fdde71",
-            name: "",
-            firstName: "Michał",
-            lastName: "Pająk",
-            department: "Fizjoterapia",
-            roles: ["Employee"],
-          },
-          {
-            id: "c188430f-3abd-4885-80f5-75e1b44f7b2b",
-            name: "Robert Lewandowski",
-            roles: ["Employee"],
-          },
-          {
-            id: "beb60a1d-a7c5-49b5-b3af-f5e43b34d2ad",
-            name: "Testowy",
-            roles: ["Employee", "DecisionMaker"],
-          },
-          {
-            id: "c73ae584-beb1-4036-8f04-6879d3b1955f",
-            name: "",
-            firstName: "Tomasz",
-            lastName: "Sojka",
-            roles: [],
-          },
-        ],
-      });
     }
   }, [inProgress]);
-  return apiUsers ?
-    (<Authorized
+  return apiUsers ? (
+    <Authorized
       roles={["UserAdmin", "GlobalAdmin"]}
-      authorized={<ManageUsersTable
-        users={apiUsers.items} />}
+      authorized={
+        <ManageUsersTable
+          users={apiUsers.items}
+          userOnChange={handleUserChange}
+        />
+      }
       unauthorized={<Forbidden />}
     />
-  ) : <Loading />;
+  ) : (
+    <Loading />
+  );
 };
 
 export const ManageUsers = () => (
--- src/web/src/ui-components/manage-users/ManageUsersTable.tsx
+++ src/web/src/ui-components/manage-users/ManageUsersTable.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/DeleteOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import CancelIcon from '@mui/icons-material/Close';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import SaveIcon from "@mui/icons-material/Save";
+import CancelIcon from "@mui/icons-material/Close";
 import {
   GridRowsProp,
   GridRowModesModel,
@@ -17,13 +17,13 @@ import {
   GridRowEditStopReasons,
   GridRenderEditCellParams,
   useGridApiContext,
-} from '@mui/x-data-grid';
-import { roleTypeNames } from '../../components/Authorized';
-import { UserDto } from '../dtos/UsersDto';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
+} from "@mui/x-data-grid";
+import { roleTypeNames } from "../../components/Authorized";
+import { UserDto } from "../dtos/UsersDto";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
-declare module '@mui/x-data-grid' {
+declare module "@mui/x-data-grid" {
   interface ToolbarPropsOverrides {
     setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
     setRowModesModel: (
@@ -32,15 +32,23 @@ declare module '@mui/x-data-grid' {
   }
 }
 
-export function ManageUsersTable(props: {users: UserDto[]}) {
-    const rowsTemp: GridRowsProp = props.users.map(x => ({
-        ...x,
-        name: x.lastName ? `${x.lastName} ${x.firstName}` : x.name
-    }));
-    const [rows, setRows] = React.useState(rowsTemp);
-    const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>({});
+export function ManageUsersTable(props: {
+  users: UserDto[];
+  userOnChange: (user: UserDto) => Promise<void>;
+}) {
+  const rowsTemp: GridRowsProp = props.users.map((x) => ({
+    ...x,
+    name: x.lastName ? `${x.lastName} ${x.firstName}` : x.name,
+  }));
+  const [rows, setRows] = React.useState(rowsTemp);
+  const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>(
+    {},
+  );
 
-  const handleRowEditStop: GridEventListener<'rowEditStop'> = (params, event) => {
+  const handleRowEditStop: GridEventListener<"rowEditStop"> = (
+    params,
+    event,
+  ) => {
     if (params.reason === GridRowEditStopReasons.rowFocusOut) {
       event.defaultMuiPrevented = true;
     }
@@ -50,18 +58,17 @@ export function ManageUsersTable(props: {users: UserDto[]}) {
     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
   };
 
-  const handleSaveClick = (id: GridRowId) => () => {
+  const handleSaveClick = (id: GridRowId) => async () => {
     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
   };
 
   const handleDeleteClick = (id: GridRowId) => () => {
     const item = rows.find((row) => row.id === id);
-    if(item) {
-        item.roles = [];
-        setRows([
-            ...rows.filter((row) => row.id !== id),
-            item
-        ]);
+    if (item) {
+      item.roles = [];
+      setRows(rows.map((row) => (row.id === id ? item : row)));
+    } else {
+      console.warn("Can't find user. (handleDeleteClick)");
     }
   };
 
@@ -70,16 +77,11 @@ export function ManageUsersTable(props: {users: UserDto[]}) {
       ...rowModesModel,
       [id]: { mode: GridRowModes.View, ignoreModifications: true },
     });
-
-    const editedRow = rows.find((row) => row.id === id);
-    if (editedRow!.isNew) {
-      setRows(rows.filter((row) => row.id !== id));
-    }
   };
 
-  const processRowUpdate = (newRow: GridRowModel) => {
-    const updatedRow = { ...newRow, isNew: false };
-    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+  const processRowUpdate = async (updatedRow: GridRowModel) => {
+    setRows(rows.map((row) => (row.id === updatedRow.id ? updatedRow : row)));
+    await props.userOnChange(updatedRow as UserDto);
     return updatedRow;
   };
 
@@ -87,28 +89,32 @@ export function ManageUsersTable(props: {users: UserDto[]}) {
     setRowModesModel(newRowModesModel);
   };
 
+  const processRowUpdateError = (e: unknown) => {
+    console.log("processRowUpdateError", e);
+  };
+
   const columns: GridColDef[] = [
-    { field: 'name', headerName: 'Name', width: 200 },
+    { field: "name", headerName: "Name", width: 200 },
     {
-      field: 'department',
-      headerName: 'Department',
+      field: "department",
+      headerName: "Department",
     },
     {
-      field: 'roles',
-      headerName: 'Roles',
+      field: "roles",
+      headerName: "Roles",
       width: 220,
       editable: true,
-      type: 'singleSelect',
+      type: "singleSelect",
       renderEditCell: CustomEditComponent,
       sortable: false,
       filterable: false,
     },
     {
-      field: 'actions',
-      type: 'actions',
-      headerName: 'Actions',
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
       width: 100,
-      cellClassName: 'actions',
+      cellClassName: "actions",
       getActions: ({ id }) => {
         const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
 
@@ -118,7 +124,7 @@ export function ManageUsersTable(props: {users: UserDto[]}) {
               icon={<SaveIcon />}
               label="Save"
               sx={{
-                color: 'primary.main',
+                color: "primary.main",
               }}
               onClick={handleSaveClick(id)}
             />,
@@ -155,12 +161,12 @@ export function ManageUsersTable(props: {users: UserDto[]}) {
     <Box
       sx={{
         height: 500,
-        width: '100%',
-        '& .actions': {
-          color: 'text.secondary',
+        width: "100%",
+        "& .actions": {
+          color: "text.secondary",
         },
-        '& .textPrimary': {
-          color: 'text.primary',
+        "& .textPrimary": {
+          color: "text.primary",
         },
       }}
     >
@@ -172,6 +178,7 @@ export function ManageUsersTable(props: {users: UserDto[]}) {
         onRowModesModelChange={handleRowModesModelChange}
         onRowEditStop={handleRowEditStop}
         processRowUpdate={processRowUpdate}
+        onProcessRowUpdateError={processRowUpdateError}
         slotProps={{
           toolbar: { setRows, setRowModesModel },
         }}
@@ -180,35 +187,34 @@ export function ManageUsersTable(props: {users: UserDto[]}) {
   );
 }
 function CustomEditComponent(props: GridRenderEditCellParams) {
-    const { id, value, field } = props;
-    const apiRef = useGridApiContext();
-  
-    const handleChange = (event: SelectChangeEvent) => {
-      const eventValue = event.target.value; // The new value entered by the user
-      console.log({ eventValue });
-      const newValue =
-        typeof eventValue === "string" ? value.split(",") : eventValue;
-      apiRef.current.setEditCellValue({
-        id,
-        field,
-        value: newValue.filter((x: string) => x !== "")
-      });
-    };
-  
-    return (
-      <Select
-        labelId="demo-multiple-name-label"
-        id="demo-multiple-name"
-        multiple
-        value={value}
-        onChange={handleChange}
-        sx={{ width: "100%" }}
-      >
-        {roleTypeNames.map((option) => (
-          <MenuItem key={option} value={option}>
-            {option}
-          </MenuItem>
-        ))}
-      </Select>
-    );
-  }
+  const { id, value, field } = props;
+  const apiRef = useGridApiContext();
+
+  const handleChange = (event: SelectChangeEvent) => {
+    const eventValue = event.target.value;
+    const newValue =
+      typeof eventValue === "string" ? value.split(",") : eventValue;
+    apiRef.current.setEditCellValue({
+      id,
+      field,
+      value: newValue.filter((x: string) => x !== ""),
+    });
+  };
+
+  return (
+    <Select
+      labelId="demo-multiple-name-label"
+      id="demo-multiple-name"
+      multiple
+      value={value}
+      onChange={handleChange}
+      sx={{ width: "100%" }}
+    >
+      {roleTypeNames.map((option) => (
+        <MenuItem key={option} value={option}>
+          {option}
+        </MenuItem>
+      ))}
+    </Select>
+  );
+}
