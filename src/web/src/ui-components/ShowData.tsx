2b0836978654e8f8321f6042c0aafee5733ef649
kbroniek <karol.broniek.gtm@gmail.com>
2024-12-06 15:48:53 +0100

    Remove unused code

--- src/web/src/ui-components/ShowData.tsx
+++
@@ -1,7 +0,0 @@
-//TODO: Delete
-export const ShowData = (props: {apiData: {[key: string]: string}}) => {
-    return (
-        <div><pre>{JSON.stringify(props.apiData, null, 2) }</pre></div>
-    );
-};
-
