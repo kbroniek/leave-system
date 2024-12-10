4e351e9da006108d69669c331e550262f8caebba
kbroniek <karol.broniek.gtm@gmail.com>
2024-12-10 10:50:13 +0100

    Fix on my leave request page

--- src/web/src/ui-components/my-leave-requests/Utils.ts
+++ src/web/src/ui-components/my-leave-requests/Utils.ts
@@ -1,8 +1,17 @@
 import { LeaveRequestDto } from "../dtos/LeaveRequestsDto";
+import { LeaveTypeCatalog, LeaveTypeDto } from "../dtos/LeaveTypesDto";
 
 export function filterLeaveRequests(
   leaveRequests: LeaveRequestDto[] | undefined,
-  leaveTypeId: string | undefined,
+  leaveTypeIds: (string | undefined)[],
 ) {
-  return leaveRequests?.filter((x) => x.leaveTypeId === leaveTypeId);
+  return leaveRequests?.filter((x) => leaveTypeIds.find(t => t === x.leaveTypeId));
+}
+
+
+export function findLeaveTypes(
+  leaveTypes: LeaveTypeDto[] | undefined,
+  leaveTypeCatalog: LeaveTypeCatalog,
+) {
+  return leaveTypes?.find((x) => x.properties?.catalog === leaveTypeCatalog);
 }
