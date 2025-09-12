e3152e46bc30f6283526d9013c0d52d9e3ac71b5
Karol Broniek <58724721+kbroniek@users.noreply.github.com>
2025-09-12 13:02:35 +0200

    Refactor ManageUsers component and table for improved user role managent (#177)
    * Refactor user ID retrieval and update role IDs in seed data
    
    - Changed user ID retrieval in UserExtensions to use the "http://schemas.microsoft.com/identity/claims/objectidentifier" claim for improved accuracy.
    - Updated role IDs in Roles.json to use UUIDs instead of email addresses, enhancing clarity in role assignments.
    - Adjusted ClaimsPrincipalExtensions to define the new IdClaim constant for consistency in user ID handling.
    
    * Refactor user retrieval logic in GetUserRepository
    
    - Updated the GetUsers method to handle empty ID arrays by directly calling GetUsersFromGraph.
    - Renamed the GetUsersLimit method to GetUsersFromGraph for clarity and consistency in user data retrieval.
    
    * Enhance user retrieval caching in GetUserRepository
    
    - Introduced IMemoryCache to cache user data for improved performance.
    - Updated GetUsers method to utilize cached results when available, reducing unnecessary API calls.
    - Added a new method, GetUsersFromGraphCache, to manage cache retrieval and storage with a 5-minute expiration policy.
    - Refactored user retrieval logic to streamline handling of user IDs and improve overall efficiency.
    
    * Refactor ManageUsers component and table for improved user role management
    
    - Added loading state management to prevent multiple updates during user role changes.
    - Refactored handleUserChange and related functions to utilize useCallback for better performance.
    - Enhanced ManageUsersTable to disable editing and actions while updating user roles.
    - Improved error handling and notifications for user updates and loading errors.
    - Cleaned up code formatting and structure for better readability and maintainability.

--- src/web/src/ui-components/manage-users/ManageUsersTable.tsx
+++ src/web/src/ui-components/manage-users/ManageUsersTable.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useCallback } from "react";
 import Box from "@mui/material/Box";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/DeleteOutlined";
@@ -37,10 +38,12 @@ declare module "@mui/x-data-grid" {
 export function ManageUsersTable(props: {
   users: UserDto[];
   userOnChange: (user: UserDto) => Promise<void>;
+  isUpdating?: boolean;
 }) {
+  const { users, userOnChange, isUpdating } = props;
   const notifications = useNotifications();
   const { t } = useTranslation();
-  const rowsTemp: GridRowsProp = props.users.map((x) => ({
+  const rowsTemp: GridRowsProp = users.map((x) => ({
     ...x,
     name: x.lastName ? `${x.lastName} ${x.firstName}` : x.name,
   }));
@@ -49,120 +52,173 @@ export function ManageUsersTable(props: {
     {},
   );
 
-  const handleRowEditStop: GridEventListener<"rowEditStop"> = (
-    params,
-    event,
-  ) => {
-    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
-      event.defaultMuiPrevented = true;
-    }
-  };
-
-  const handleEditClick = (id: GridRowId) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
-  };
-
-  const handleSaveClick = (id: GridRowId) => async () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-  };
-
-  const handleDeleteClick = (id: GridRowId) => () => {
-    const item = rows.find((row) => row.id === id);
-    if (item) {
-      item.roles = [];
-      setRows(rows.map((row) => (row.id === id ? item : row)));
-    } else {
-      console.warn("Can't find user. (handleDeleteClick)");
-    }
-  };
-
-  const handleCancelClick = (id: GridRowId) => () => {
-    setRowModesModel({
-      ...rowModesModel,
-      [id]: { mode: GridRowModes.View, ignoreModifications: true },
-    });
-  };
-
-  const processRowUpdate = async (updatedRow: GridRowModel) => {
-    setRows(rows.map((row) => (row.id === updatedRow.id ? updatedRow : row)));
-    await props.userOnChange(updatedRow as UserDto);
-    return updatedRow;
-  };
-
-  const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
-    setRowModesModel(newRowModesModel);
-  };
-
-  const processRowUpdateError = (e: unknown) => {
-    console.warn("processRowUpdateError", e);
-    notifications.show(t("Something went wrong when updating row."), {
-      severity: "error",
-    });
-  };
-
-  const columns: GridColDef[] = [
-    { field: "name", headerName: t("Name"), width: 200 },
-    {
-      field: "jobTitle",
-      headerName: t("Job title"),
+  const handleRowEditStop: GridEventListener<"rowEditStop"> = useCallback(
+    (params, event) => {
+      if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+        event.defaultMuiPrevented = true;
+      }
     },
-    {
-      field: "roles",
-      headerName: t("Roles"),
-      width: 220,
-      editable: true,
-      type: "singleSelect",
-      renderEditCell: CustomEditComponent,
-      sortable: false,
-      filterable: false,
-    },
-    {
-      field: "actions",
-      type: "actions",
-      headerName: t("Actions"),
-      width: 100,
-      cellClassName: "actions",
-      getActions: ({ id }) => {
-        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+    [],
+  );
 
-        if (isInEditMode) {
+  const handleEditClick = useCallback(
+    (id: GridRowId) => () => {
+      setRowModesModel((prevModel) => ({
+        ...prevModel,
+        [id]: { mode: GridRowModes.Edit },
+      }));
+    },
+    [],
+  );
+
+  const handleSaveClick = useCallback(
+    (id: GridRowId) => async () => {
+      if (isUpdating) return; // Prevent multiple saves
+      setRowModesModel((prevModel) => ({
+        ...prevModel,
+        [id]: { mode: GridRowModes.View },
+      }));
+    },
+    [isUpdating],
+  );
+
+  const handleDeleteClick = useCallback(
+    (id: GridRowId) => () => {
+      if (isUpdating) return; // Prevent actions while updating
+      setRows((prevRows) => {
+        const item = prevRows.find((row) => row.id === id);
+        if (item) {
+          const updatedItem = { ...item, roles: [] };
+          return prevRows.map((row) => (row.id === id ? updatedItem : row));
+        } else {
+          console.warn("Can't find user. (handleDeleteClick)");
+          return prevRows;
+        }
+      });
+    },
+    [isUpdating],
+  );
+
+  const handleCancelClick = useCallback(
+    (id: GridRowId) => () => {
+      setRowModesModel((prevModel) => ({
+        ...prevModel,
+        [id]: { mode: GridRowModes.View, ignoreModifications: true },
+      }));
+    },
+    [],
+  );
+
+  const processRowUpdate = useCallback(
+    async (updatedRow: GridRowModel) => {
+      setRows((prevRows) =>
+        prevRows.map((row) => (row.id === updatedRow.id ? updatedRow : row)),
+      );
+      await userOnChange(updatedRow as UserDto);
+      return updatedRow;
+    },
+    [userOnChange],
+  );
+
+  const handleRowModesModelChange = useCallback(
+    (newRowModesModel: GridRowModesModel) => {
+      setRowModesModel(newRowModesModel);
+    },
+    [],
+  );
+
+  const processRowUpdateError = useCallback(
+    (e: unknown) => {
+      console.warn("processRowUpdateError", e);
+      notifications.show(t("Something went wrong when updating row."), {
+        severity: "error",
+        autoHideDuration: 5000,
+      });
+    },
+    [notifications, t],
+  );
+
+  const columns: GridColDef[] = React.useMemo(
+    () => [
+      { field: "name", headerName: t("Name"), width: 200 },
+      {
+        field: "jobTitle",
+        headerName: t("Job title"),
+      },
+      {
+        field: "roles",
+        headerName: t("Roles"),
+        width: 220,
+        editable: !isUpdating, // Disable editing while updating
+        type: "singleSelect",
+        renderEditCell: CustomEditComponent,
+        sortable: false,
+        filterable: false,
+      },
+      {
+        field: "actions",
+        type: "actions",
+        headerName: t("Actions"),
+        width: 100,
+        cellClassName: "actions",
+        getActions: ({ id }) => {
+          const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+          if (isInEditMode) {
+            return [
+              <GridActionsCellItem
+                key="save"
+                icon={<SaveIcon />}
+                label={t("Save")}
+                disabled={isUpdating}
+                sx={{
+                  color: isUpdating ? "grey.500" : "primary.main",
+                }}
+                onClick={handleSaveClick(id)}
+              />,
+              <GridActionsCellItem
+                key="cancel"
+                icon={<CancelIcon />}
+                label={t("Cancel")}
+                className="textPrimary"
+                onClick={handleCancelClick(id)}
+                color="inherit"
+              />,
+            ];
+          }
+
           return [
             <GridActionsCellItem
-              icon={<SaveIcon />}
-              label={t("Save")}
-              sx={{
-                color: "primary.main",
-              }}
-              onClick={handleSaveClick(id)}
+              key="edit"
+              icon={<EditIcon />}
+              label={t("Edit")}
+              className="textPrimary"
+              disabled={isUpdating}
+              onClick={handleEditClick(id)}
+              color="inherit"
             />,
             <GridActionsCellItem
-              icon={<CancelIcon />}
-              label={t("Cancel")}
-              className="textPrimary"
-              onClick={handleCancelClick(id)}
+              key="delete"
+              icon={<DeleteIcon />}
+              label={t("Delete")}
+              disabled={isUpdating}
+              onClick={handleDeleteClick(id)}
               color="inherit"
             />,
           ];
-        }
-
-        return [
-          <GridActionsCellItem
-            icon={<EditIcon />}
-            label={t("Edit")}
-            className="textPrimary"
-            onClick={handleEditClick(id)}
-            color="inherit"
-          />,
-          <GridActionsCellItem
-            icon={<DeleteIcon />}
-            label={t("Delete")}
-            onClick={handleDeleteClick(id)}
-            color="inherit"
-          />,
-        ];
+        },
       },
-    },
-  ];
+    ],
+    [
+      t,
+      isUpdating,
+      rowModesModel,
+      handleSaveClick,
+      handleCancelClick,
+      handleEditClick,
+      handleDeleteClick,
+    ],
+  );
 
   return (
     <Box
